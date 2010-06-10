a6fd0774f0485d0817de27f7047d4d8306118c23
Gavin Norman <gavin.norman@sociomantic.com>
2010-06-10 10:09:12 +0000

    + added QueueMemory unittest

--- test/main.d
+++ test/main.d
@@ -3,6 +3,7 @@ module main;
 import ocean.core.ArrayMap;
 import ocean.io.digest.Fnv1;
 import ocean.io.Retry;
+import ocean.io.device.QueueMemory;
 import ocean.net.http.Url;
 import ocean.time.UnixTime;
 
