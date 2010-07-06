30bda99fc7175d01c96895f51a92af19b2939666
Gavin Norman <gavin.norman@sociomantic.com>
2010-07-06 16:34:32 +0000

    + added ocean.core.ObjectThreadPool unittest

--- test/main.d
+++ test/main.d
@@ -1,10 +1,15 @@
 module main;
 
 import ocean.core.ArrayMap;
+import ocean.core.ObjectThreadPool;
+
 import ocean.io.digest.Fnv1;
+
 import ocean.io.Retry;
 import ocean.io.device.QueueMemory;
+
 import ocean.net.http.Url;
+
 import ocean.time.UnixTime;
 
 void main ()
