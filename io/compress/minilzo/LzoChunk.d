803156537fa33e58c70735b90ac466908c76e2f0
David Eckardt <david.eckardt@sociomantic.com>
2010-07-28 19:09:07 +0000

    + LZO compression

---
+++ io/compress/minilzo/LzoChunk.d
@@ -0,0 +1,397 @@
+/******************************************************************************
+
+    LZO1X-1 (Mini LZO) compressor/uncompressor generating/accepting chunks of
+    compressed data with a length and checksum header 
+
+    copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
+    
+    version:        July 2010: Initial release
+    
+    authors:        David Eckardt
+    
+    LZO chunk data layout if size_t has a width of 32-bit:
+        void[] chunk
+        
+        chunk[0 .. 17] - header
+        chunk[17 .. $] - compressed data
+        
+        Header data layout:
+            chunk[0  ..  4] - length of chunk[4 .. $] (or compressed data length
+                              + header length - 4)
+            chunk[4  ..  9] - algorithm identifier, e.g. "LZO1X"
+            chunk[9  .. 13] - length of uncompressed data
+            chunk[13 .. 17] - 32-bit CRC value of compressed data, calculated
+                              using lzo_crc32()
+            
+ ******************************************************************************/
+
+module ocean.io.compress.minilzo.LzoChunk;
+
+/******************************************************************************
+
+    Imports
+    
+******************************************************************************/
+
+private import ocean.io.compress.minilzo.MiniLzo;
+
+private import ocean.core.Exception: CompressException, assertEx;
+
+/******************************************************************************
+
+    LzoChunk class
+
+ ******************************************************************************/
+
+class LzoChunk
+{
+    /**************************************************************************
+    
+        MiniLzo instance
+         
+     **************************************************************************/
+
+    private MiniLzo lzo;
+    
+    /**************************************************************************
+    
+        Data buffer
+         
+     **************************************************************************/
+
+    private void[] data;
+    
+    /**************************************************************************
+    
+        Constructor
+        
+        Params:
+            data_size = expected uncompressed data size (optional, for
+                        preallocation)
+         
+     **************************************************************************/
+
+    public this ( size_t data_size = 0 )
+    {
+        this.lzo = new MiniLzo;
+        
+        this.data = new void[Header.length + this.lzo.maxCompressedLength(data_size)];
+    }
+    
+    /**************************************************************************
+    
+        Compresses a data chunk 
+        
+        Params:
+            uncompressed = data chunk to compress
+            
+        Returns:
+            LZO chunk containing compressed data
+         
+     **************************************************************************/
+
+    public void[] compress ( void[] uncompressed )
+    {
+        this.data.length = Header.length + this.lzo.maxCompressedLength(uncompressed.length);
+        
+        size_t end = Header.length + this.lzo.compress(uncompressed, this.data[Header.length .. $]);
+        
+        this.data.length = end;
+        
+        return Header.write(this.data, data.length);
+    }
+    
+    /**************************************************************************
+    
+        Uncompresses a LZO chunk 
+        
+        Params:
+            chunk = LZO chunk to uncompress
+            
+        Returns:
+            uncompressed data chunk
+         
+     **************************************************************************/
+
+    public void[] uncompress ( void[] chunk )
+    {
+        size_t uncompressed_length;
+        
+        void[] compressed = Header.read(chunk, uncompressed_length);
+        
+        this.data.length = uncompressed_length;
+        
+        this.lzo.decompress(compressed, this.data);
+        
+        return this.data;
+    }
+    
+    /**************************************************************************
+    
+        Destructor
+         
+     **************************************************************************/
+
+    ~this ( )
+    {
+        delete this.lzo;
+        delete this.data;
+    }
+    
+    /**************************************************************************
+    
+        Calculates the sum of the sizes of the types of T
+         
+     **************************************************************************/
+
+    template SizeofTuple ( T ... )
+    {
+        static if (T.length > 1)
+        {
+            const SizeofTuple = T[0].sizeof + SizeofTuple!(T[1 .. $]);
+        }
+        else
+        {
+            const SizeofTuple = T[0].sizeof;
+        }
+    }
+
+    /**************************************************************************
+    
+        Header structure
+         
+     **************************************************************************/
+
+    align (1) struct Header
+    {
+        /**********************************************************************
+            
+            Length of the chunk excluding this length value
+             
+         **********************************************************************/
+        
+        size_t chunk_length;
+        
+        /**********************************************************************
+        
+            Algorithm identifier. AlgoId is defined below
+             
+         **********************************************************************/
+        
+        char[5] algo_id = AlgoId;
+        
+        /**********************************************************************
+        
+            Length of uncompressed data
+             
+         **********************************************************************/
+        
+        size_t uncompressed_length;
+        
+        /**********************************************************************
+        
+            CRC32 of compressed data
+             
+         **********************************************************************/
+        
+        uint crc32;
+        
+        /**********************************************************************
+        
+            Identifer of LZO algorithm used
+             
+         **********************************************************************/
+    
+        const AlgoId = "LZO1X";
+        
+        /**********************************************************************
+        
+            Total data length of the members of this structure. With "align (1)"
+            as structure definition attribute "length" equals the "sizeof" value
+            since the member data are then packed without padding.
+            Because this structure represents the LZO chunk header data
+            elements, "length" must equal "sizeof" in order to generate the
+            correct LZO chunk header by serializing an instance of this
+            structure. Hence "length == sizeof" is checked at ccompile-time
+            in write(). 
+             
+         **********************************************************************/
+
+        const length = SizeofTuple!(typeof (typeof (*this).tupleof));
+        
+        /**********************************************************************
+        
+            Sets the header elements according to compressed and
+            uncompressed_length.
+            
+            Params:
+                compressed          = compressed data
+                uncompressed_length = data length before compression 
+             
+            Returns:
+                this instance
+             
+         **********************************************************************/
+
+        typeof (this) set ( void[] compressed, size_t uncompressed_length )
+        {
+            this.chunk_length = compressed.length + this.length - typeof (this.chunk_length).sizeof;
+            
+            this.uncompressed_length = uncompressed_length;
+            
+            this.crc32 = MiniLzo.crc32(compressed);
+            
+            return this;
+        }
+        
+        /**********************************************************************
+        
+            Writes the header to chunk[0 .. this.length].
+            
+            Params:
+                chunk = LZO chunk without header
+             
+            Returns:
+                chunk (passed through)
+             
+            Throws:
+                CompressException if chunk is shorter than this.length
+             
+         **********************************************************************/
+
+        void[] write ( void[] chunk )
+        {
+            static assert (typeof (*this).sizeof == this.length, typeof (*this).stringof ~ ": bad data alignment");
+            
+            assertEx!(CompressException)(chunk.length >= this.length, "LZO chunk too short too write a header");
+            
+            *(cast (typeof (this)) chunk.ptr) = *this; 
+            
+            return chunk;
+        }
+        
+        /**********************************************************************
+        
+            Reads the header from chunk and sets the members of this instance to
+            the values contained in the header.
+            
+            Params:
+                chunk = LZO chunk with header
+             
+            Returns:
+                this instance
+             
+            Throws:
+                CompressException if chunk is shorter than this.length
+                
+         **********************************************************************/
+
+        typeof (this) read ( void[] chunk )
+        {
+            assertEx!(CompressException)(chunk.length >= this.length, "LZO chunk too short too read a header");
+            
+            *this = *(cast (typeof (this)) chunk.ptr);
+            
+            return this;
+        }
+        
+        /**********************************************************************
+        
+            Checks if the member values of this instance are correct and
+            consistent with the compressed data.
+            
+            Params:
+                compressed = compressed data
+             
+            Returns:
+                compressed data (passed through)
+             
+            Throws:
+                CompressException if one of the following conditions is not
+                satisfied:
+                    1. compressed.length is not equal to
+                       this.chunk_length - header length
+                    2. The algorithm ID does not match this.AlgoId
+                    3. The compressed data lenght exceeds the possible maximum 
+                       compressed data length calculated from
+                       this.uncompressed_length
+                    4. The 32-bit CRC value calculated from compressed does not
+                       equal this.crc32
+                
+         **********************************************************************/
+
+        void[] check ( void[] compressed )
+        {
+            assertEx(compressed.length == this.chunk_length - this.length + typeof (this.chunk_length).sizeof, "LZO chunk size mismatch");
+            
+            assertEx!(CompressException)(this.algo_id == this.AlgoId, "Wrong algorithm in LZO chunk header");
+            
+            assertEx!(CompressException)(this.uncompressed_length <= MiniLzo.maxCompressedLength(compressed.length), "LZO compressed data too long");
+            
+            assertEx!(CompressException)(this.crc32 == MiniLzo.crc32(compressed), "LZO chunk data corrupted (CRC32 mismatch)");
+            
+            return compressed;
+        }
+        
+        /**********************************************************************
+        
+            Sets the header elements according to chunk and uncompressed_length
+            and writes the header to chunk[0 .. this.length].
+            chunk[this.length .. $] contains compressed data whose
+            length was uncompressed_length before compression.
+            
+            Params:
+                chunk               = LZO chunk without header
+                uncompressed_length = data length before compression
+             
+            Returns:
+                chunk (passed through)
+             
+             Throws:
+                CompressException if chunk is shorter than this.length
+             
+         **********************************************************************/
+
+       static void[] write ( void[] chunk, size_t uncompressed_length )
+        {
+            typeof (*this) header;
+            
+            return header.set(chunk[this.length .. $], uncompressed_length).write(chunk);
+        }
+        
+       /**********************************************************************
+       
+           Strips the header from chunk and checks if the header element values
+           of this instance are correct and consistent with the compressed data.
+           
+           Params:
+               chunk = LZO chunk with header
+            
+           Returns:
+               compressed data (slice of chunk)
+            
+           Throws:
+                CompressException if one of the following conditions is not
+                satisfied:
+                    1. chunk is shorter than this.length
+                    2. compressed.length is not equal to
+                       this.chunk_length - header length
+                    3. The algorithm ID does not match this.AlgoId
+                    4. The compressed data lenght exceeds the possible maximum 
+                       compressed data length calculated from
+                       this.uncompressed_length
+                    5. The 32-bit CRC value calculated from compressed does not
+                       equal this.crc32
+               
+        **********************************************************************/
+
+        static void[] read ( void[] chunk, out size_t uncompressed_length )
+        {
+            typeof (*this) header;
+            
+            scope (success) uncompressed_length = header.uncompressed_length;
+            
+            return header.read(chunk).check(chunk[this.length .. $]);
+        }
+    }
+}
---
+++ io/compress/minilzo/MiniLzo.d
@@ -0,0 +1,267 @@
+/******************************************************************************
+
+    LZO1X-1 (Mini LZO) compressor/uncompressor 
+
+    copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
+    
+    version:        July 2010: Initial release
+    
+    authors:        David Eckardt
+            
+ ******************************************************************************/
+
+module ocean.io.compress.minilzo.MiniLzo;
+
+/******************************************************************************
+
+    Imports
+
+ ******************************************************************************/
+
+private import ocean.io.compress.minilzo.c.minilzo : lzo1x_1_compress,
+                                                     lzo1x_decompress, lzo1x_decompress_safe,
+                                                     lzo1x_max_compressed_length, lzo_init, lzo_crc32, lzo_crc32_init,
+                                                     Lzo1x1WorkmemSize, LzoStatus;
+
+private import ocean.core.Exception: CompressException, assertEx;
+
+/******************************************************************************
+
+    MiniLzo class
+
+ ******************************************************************************/
+
+class MiniLzo
+{
+    /**************************************************************************
+
+        Working memory buffer for lzo1x_1_compress()
+    
+     **************************************************************************/
+
+    private void[] workmem;
+    
+    /**************************************************************************
+
+        Static constructor
+        
+        Throws:
+            CompressException if the library pouts
+        
+     **************************************************************************/
+
+    static this ( )
+    {
+        assertEx!(CompressException)(!lzo_init(), "MiniLzo kaputt");
+    }
+
+    /**************************************************************************
+
+        Constructor
+    
+     **************************************************************************/
+
+    public this ( )
+    {
+        this.workmem = new void[Lzo1x1WorkmemSize];
+    }
+    
+    /**************************************************************************
+
+        Compresses src data. dst must have a length of at least
+        maxCompressedLength(src.length).
+        
+        Params:
+            src = data to compress
+            dst = compressed data destination buffer
+        
+        Returns:
+            length of compressed data in dst
+
+        Throws:
+            CompressionException on error
+        
+     **************************************************************************/
+
+    size_t compress ( void[] src, void[] dst )
+    in
+    {
+        assert (dst.length >= this.maxCompressedLength(src.length), typeof (this).stringof ~ ".compress: dst buffer too short");
+    }
+    body
+    {
+        size_t len;
+        
+        this.checkStatus(lzo1x_1_compress(cast (ubyte*) src.ptr, src.length, cast (ubyte*) dst.ptr, &len, this.workmem.ptr));
+        
+        return len;
+    }
+        
+    /**************************************************************************
+
+        Uncompresses src data. dst must have at least the length of the
+        uncompressed data, which must be memorized at compression time.
+        
+        Note: dst overflow checking is NOT done!
+        
+        Params:
+            src = data to uncompress
+            dst = uncompressed data destination buffer
+        
+        Returns:
+            length of uncompressed data in dst
+        
+        Throws:
+            CompressionException on error
+            
+     **************************************************************************/
+
+    size_t decompress ( void[] src, void[] dst )
+    {
+        size_t len;
+        
+        this.checkStatus(lzo1x_decompress(cast (ubyte*) src.ptr, src.length, cast (ubyte*) dst.ptr, &len));
+        
+        return len;
+    }
+    
+    /**************************************************************************
+
+        Uncompresses src data, checking for dst not to overflow.
+        
+        Params:
+            src = data to uncompress
+            dst = uncompressed data destination buffer
+        
+        Returns:
+            length of uncompressed data in dst
+        
+        Throws:
+            CompressionException on error
+        
+     **************************************************************************/
+
+    size_t decompressSafe ( void[] src, void[] dst )
+    {
+        size_t len;
+        
+        this.checkStatus(lzo1x_decompress_safe(cast (ubyte*) src.ptr, src.length, cast (ubyte*) dst.ptr, &len));
+        
+        return len;
+    }
+    
+    /******************************************************************************
+
+        Calculates the maximum compressed length of data which has a length of
+        uncompressed_length.
+        
+        Note: Surprisingly, this is more than uncompressed_length but that's the
+              worst case for completely uncompressable data.
+    
+        Parameters:
+            uncompressed_length = length of data to compressed
+            
+        Returns:
+            maximum compressed length of data
+    
+     ******************************************************************************/
+
+    static size_t maxCompressedLength ( size_t uncompressed_length )
+    {
+        return lzo1x_max_compressed_length(uncompressed_length);
+    }
+    
+    /**************************************************************************
+    
+        Calculates a 32-bit CRC value from data.
+        
+        Params:
+            crc32_in = initial 32-bit CRC value (for iteration)
+            data     = data to calculate 32-bit CRC value of
+            
+        Returns:
+            resulting 32-bit CRC value
+    
+     **************************************************************************/
+
+    static uint crc32 ( uint crc32_in, void[] data )
+    {
+        return lzo_crc32(crc32_in, cast (ubyte*) data.ptr, data.length);
+    }
+    
+    /**************************************************************************
+    
+        Calculates a 32-bit CRC value from data.
+        
+        Params:
+            data = data to calculate 32-bit CRC value of
+            
+        Returns:
+            resulting 32-bit CRC value
+    
+     **************************************************************************/
+
+    static uint crc32 ( void[] data )
+    {
+        return crc32(lzo_crc32_init(), data);
+    }
+    
+    /**************************************************************************
+    
+        Checks if status indicates an error.
+        
+        Params:
+            status = LZO library function return status
+            
+        Throws:
+            resulting 32-bit CRC value
+    
+     **************************************************************************/
+
+    static void checkStatus ( LzoStatus status )
+    {
+        switch (status)
+        {
+            case LzoStatus.Error:
+                throw new CompressException(typeof (this).stringof ~ ": Error");
+                
+            case LzoStatus.OutOfMemory:
+                throw new CompressException(typeof (this).stringof ~ ": Out Of Memory");
+                
+            case LzoStatus.NotCompressible:
+                throw new CompressException(typeof (this).stringof ~ ": Not Compressible");
+                
+            case LzoStatus.InputOverrun:
+                throw new CompressException(typeof (this).stringof ~ ": Input Overrun");
+                
+            case LzoStatus.OutputOverrun:
+                throw new CompressException(typeof (this).stringof ~ ": Output Overrun");
+                
+            case LzoStatus.LookBehindOverrun:
+                throw new CompressException(typeof (this).stringof ~ ": Look Behind Overrun");
+                
+            case LzoStatus.EofNotFound:
+                throw new CompressException(typeof (this).stringof ~ ": Eof Not Found");
+                
+            case LzoStatus.InputNotConsumed:
+                throw new CompressException(typeof (this).stringof ~ ": Input Not Consumed");
+                
+            case LzoStatus.NotYetImplemented:
+                throw new CompressException(typeof (this).stringof ~ ": Not Yet Implemented");
+                
+            default:
+                return;
+        }
+    }
+    
+    /**************************************************************************
+    
+        Destructor
+    
+     **************************************************************************/
+
+   ~this ( )
+    {
+        delete this.workmem;
+    }
+}
