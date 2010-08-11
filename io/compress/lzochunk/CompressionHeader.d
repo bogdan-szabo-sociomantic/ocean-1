3cee69f7fefffa1c16ba01b3b30d1f57ccb2ee0c
David Eckardt <david.eckardt@sociomantic.com>
2010-08-11 12:41:55 +0000

    + unittest for CompressionHeader + -debug=GcDisabled disables the garbage collector in unittests

--- io/compress/lzochunk/CompressionHeader.d
+++ io/compress/lzochunk/CompressionHeader.d
@@ -316,7 +316,7 @@ align (1) struct CompressionHeader ( bool LengthInline = true )
         {
             this.setHeader(chunk);
             
-            debug Trace.formatln("Not NULL, len={}, crc={}, type={}, uncompressed len={}", this.chunk_length, this.crc32_, this.type, this.uncompressed_length);
+//            debug Trace.formatln("Not NULL, len={}, crc={}, type={}, uncompressed len={}", this.chunk_length, this.crc32_, this.type, this.uncompressed_length);
             
             if (this.type == Type.Start)
             {
@@ -540,3 +540,166 @@ template SizeofTuple ( T ... )
     }
 }
 
+/******************************************************************************
+
+    Unit test
+
+    Add -debug=GcDisabled to the compiler command line to disable the garbage
+    collector.
+    
+ ******************************************************************************/
+
+
+debug (CompressionHeaderUnitTest):
+    
+import tango.util.log.Trace;
+
+import tango.time.StopWatch;
+
+import ocean.text.util.MetricPrefix;
+
+debug (GcDisabled) import tango.core.internal.gcInterface: gc_disable;
+
+unittest
+{
+    debug (GcDisabled)
+    {
+        pragma (msg, "CompressionHeader unittest: garbage collector disabled");
+        gc_disable();
+    }
+    
+    StopWatch swatch;
+    
+    CompressionHeader!() header;
+    
+    const N = 1000;
+    
+    ubyte[header.length][N] start_header_data, stop_header_data;
+    
+    const C = 10;
+    
+    scope chunks4k  = new void[][](C, 0x1000);
+    scope chunks64k = new void[][](C, 0x1_0000);
+    scope chunks1M  = new void[][](C, 0x10_0000);
+    
+    foreach (ref chunk; chunks4k)
+    {
+        (cast (char[]) chunk)[] = 'A';
+    }
+    
+    foreach (ref chunk; chunks64k)
+    {
+        (cast (char[]) chunk)[] = 'B';
+    }
+    
+    foreach (ref chunk; chunks1M)
+    {
+        (cast (char[]) chunk)[] = 'C';
+    }
+    
+    swatch.start();
+    
+    for (uint i = 0; i < N; i++)
+    {
+        start_header_data[i][] = cast (ubyte[]) header.start(0x1000).data;
+    }
+    
+    ulong us_start = swatch.microsec();
+    
+    for (uint i = 0; i < N; i++)
+    {
+        stop_header_data[i][] = cast (ubyte[]) header.stop().data;
+    }
+    
+    ulong us_stop = swatch.microsec();
+    
+    for (uint i = 0; i < N; i++)
+    {
+        header.tryReadStart(start_header_data[i]);
+    }
+    
+    ulong us_try_read_start = swatch.microsec();
+    
+    for (uint i = 0; i < N; i++)
+    {
+        header.readStart(start_header_data[i]);
+    }
+    
+    ulong us_read_start = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.write(chunks4k[i]);
+    }
+    
+    ulong us_write4k = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.write(chunks64k[i]);
+    }
+    
+    ulong us_write64k = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.write(chunks1M[i]);
+    }
+    
+    ulong us_write1M = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.read(chunks4k[i]);
+    }
+    
+    ulong us_read4k = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.read(chunks64k[i]);
+    }
+    
+    ulong us_read64k = swatch.microsec();
+    
+    for (uint i = 0; i < C; i++)
+    {
+        header.read(chunks1M[i]);
+    }
+    
+    ulong us_read1M = swatch.microsec();
+    
+    us_read1M         -= us_read64k;
+    us_read64k        -= us_read4k;
+    us_read4k         -= us_write1M;
+    us_write1M        -= us_write64k;
+    us_write64k       -= us_write4k;
+    us_write4k        -= us_read_start;
+    us_read_start     -= us_try_read_start;
+    us_try_read_start -= us_stop;
+    us_stop           -= us_start;
+    
+    Trace.formatln("CompressionHeader unittest results:\n\t"
+                   "start():        1000 headers generated within {} ms\n\t"
+                   "stop():         1000 headers generated within {} ms\n\t"
+                   "tryReadStart(): 1000 headers checked   within {} ms\n\t"
+                   "readStart():    1000 headers checked   within {} ms\n\t"
+                   "\n\t"
+                   "write(): 10 chunks of  4 kB each written within {} ms\n\t"
+                   "write(): 10 chunks of 64 kB each written within {} ms\n\t"
+                   "write(): 10 chunks of  1 MB each written within {} ms\n\t"
+                   "\n\t"
+                   "read():  10 chunks of  4 kB each read    within {} ms\n\t"
+                   "read():  10 chunks of 64 kB each read    within {} ms\n\t"
+                   "read():  10 chunks of  1 MB each read    within {} ms\n",
+                   us_start          / 1000.f,
+                   us_stop           / 1000.f,
+                   us_try_read_start / 1000.f,
+                   us_read_start     / 1000.f,
+                   us_write4k        / 1000.f,
+                   us_write64k       / 1000.f,
+                   us_write1M        / 1000.f,
+                   us_read4k         / 1000.f, 
+                   us_read64k        / 1000.f,
+                   us_read1M         / 1000.f);
+}
