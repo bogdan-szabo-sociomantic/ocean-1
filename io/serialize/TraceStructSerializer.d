6be1034e5f526632491396bc47050f90c8e387f8
Gavin Norman <gavin.norman@sociomantic.com>
2010-11-16 11:57:14 +0000

    * changed to use StringStructSerializer internally - this class is now just a convenience wrapper really

--- io/serialize/TraceStructSerializer.d
+++ io/serialize/TraceStructSerializer.d
@@ -11,12 +11,15 @@
 
     Serializer, to be used with the StructSerializer in
     ocean.io.serialize.StructSerializer, which dumps a struct to Trace.
+    
+    The serializer uses the StringStructSerializer internally, and just writes
+    the output strings to Trace.
 
     Usage example (in conjunction with ocean.io.serialize.StructSerializer):
     
     ---
 
-        // Example struct to serizlie to Trace
+        // Example struct to serialize to Trace
         struct Data
         {
             struct Id
@@ -36,7 +39,7 @@
         test.ids = [Data.Id("hi", 23), Data.Id("hello", 17)];
 
         // Create serializer object
-        scope ser = new TraceStructSerializer!(char)();
+        scope ser = new TraceStructSerializer();
 
         // Dump struct to Trace via serializer
         ser.serialize(data);
@@ -56,6 +59,7 @@ module ocean.io.serialize.TraceStructSerializer;
 *******************************************************************************/
 
 private import ocean.io.serialize.StructSerializer;
+private import ocean.io.serialize.StringStructSerializer;
 
 private import tango.util.log.Trace;
 
@@ -64,27 +68,51 @@ private import tango.util.log.Trace;
 /*******************************************************************************
 
     Trace struct serializer
-    
+
 *******************************************************************************/
 
 class TraceStructSerializer
 {
     /***************************************************************************
 
-        Indentation size
+        String serializer
     
     ***************************************************************************/
 
-    private const indent_size = 3;
+    private StringStructSerializer!(char) string_serializer;
 
 
     /***************************************************************************
 
-        Indentation level string - filled with spaces.
+        String to receive serialized data
+    
+    ***************************************************************************/
+    
+    private char[] string;
+
+
+    /***************************************************************************
+
+        Constructor
     
     ***************************************************************************/
 
-    private char[] indent;
+    public this ( )
+    {
+        this.string_serializer = new StringStructSerializer!(char)();
+    }
+
+
+    /***************************************************************************
+
+        Destructor
+    
+    ***************************************************************************/
+
+    ~this ( )
+    {
+        delete this.string_serializer;
+    }
 
 
     /***************************************************************************
@@ -106,162 +134,123 @@ class TraceStructSerializer
 
 
     /***************************************************************************
-
-        Called at the start of struct serialization - opens the xml string with
-        the required xml header and the open tag of the top-level object.
-
+    
+        Called at the start of struct serialization - outputs the name of the
+        top-level object.
+    
         Params:
-            output = string to serialize xml data to
             name = name of top-level object
     
     ***************************************************************************/
-
+    
     void open ( char[] name )
     {
-        Trace.formatln("{}struct {}:", this.indent, name);
-        this.increaseIndent();
+        this.string.length = 0;
+        this.string_serializer.open(this.string, name);
+        Trace.format("{}", this.string).flush();
     }
-
-
+    
+    
     /***************************************************************************
-
-        Called at the end of struct serialization - closes the xml string with
-        a close tag for the top-level object
+    
+        Called at the end of struct serialization
     
         Params:
-            output = string to serialize xml data to
             name = name of top-level object
-
+    
     ***************************************************************************/
-
+    
     void close ( char[] name )
     {
-        this.decreaseIndent();
+        this.string.length = 0;
+        this.string_serializer.close(this.string, name);
+        Trace.format("{}", this.string).flush();
     }
-
-
+    
+    
     /***************************************************************************
-
-        Appends a named item to the xml string
-
+    
+        Writes a named item to Trace
+    
         Template params:
             T = type of item
         
         Params:
-            output = string to serialize xml data to
             item = item to append
             name = name of item
     
     ***************************************************************************/
-
+    
     void serialize ( T ) ( ref T item, char[] name )
     {
-        Trace.formatln("{}{} {} : {}", this.indent, T.stringof, name, item);
+        this.string.length = 0;
+        this.string_serializer.serialize(this.string, item, name);
+        Trace.format("{}", this.string).flush();
     }
-
-
+    
+    
     /***************************************************************************
-
-        Appends a struct to the xml string (as a named object)
+    
+        Writes a struct to Trace
     
         Params:
-            output = string to serialize xml data to
             name = name of struct item
             serialize_struct = delegate which is expected to call further
                 methods of this class in order to serialize the struct's
                 contents
-
+    
     ***************************************************************************/
-
+    
     void serializeStruct ( char[] name, void delegate ( ) serialize_struct )
     {
-        Trace.formatln("{}struct {}:", this.indent, name);
-        this.increaseIndent();
-        serialize_struct();
-        this.decreaseIndent();
+        this.string.length = 0;
+        this.string_serializer.serializeStruct(this.string, name, serialize_struct);
+        Trace.format("{}", this.string).flush();
     }
-
+    
     
     /***************************************************************************
-
-        Appends a named array to the xml string
-
+    
+        Writes a named array to Trace
+    
         Template params:
             T = base type of array
     
         Params:
-            output = string to serialize xml data to
             array = array to append
             name = name of array item
-
+    
     ***************************************************************************/
-
+    
     void serializeArray ( T ) ( T[] array, char[] name )
     {
-        Trace.formatln("{}{}[] {} (length {}): {}", this.indent, T.stringof, name, array.length, array);
+        this.string.length = 0;
+        this.string_serializer.serializeArray(this.string, array, name);
+        Trace.format("{}", this.string).flush();
     }
-
+    
     
     /***************************************************************************
-
-        Appends a named array of structs to the xml string, as an array of
-        indexed objects.
+    
+        Writes a named array of structs to Trace.
     
         Template params:
             T = base type of array
     
         Params:
-            output = string to serialize xml data to
             array = array to append
             name = name of struct item
             serialize_element = delegate which is expected to call further
                 methods of this class in order to serialize each struct's
                 contents
-
+    
     ***************************************************************************/
-
+    
     void serializeStructArray ( T ) ( char[] name, T[] array, void delegate ( ref T ) serialize_element )
     {
-        Trace.formatln("{}{}[] {} (length {}):", this.indent, T.stringof, name, array.length);
-        this.increaseIndent();
-
-        foreach ( i, item; array )
-        {
-            serializeStruct(T.stringof, { serialize_element(item); });
-        }
-
-        this.decreaseIndent();
-    }
-
-
-    /***************************************************************************
-
-        Increases the indentation level.
-    
-    ***************************************************************************/
-
-    private void increaseIndent ( )
-    {
-        this.indent.length = this.indent.length + indent_size;
-        this.indent[] = ' ';
-    }
-    
-
-    /***************************************************************************
-
-        Decreases the indentation level.
-    
-    ***************************************************************************/
-
-    private void decreaseIndent ( )
-    in
-    {
-        assert(this.indent.length >= indent_size, typeof(this).stringof ~ ".decreaseIndent - indentation cannot be decreased");
-    }
-    body
-    {
-        this.indent.length = this.indent.length - indent_size;
-        this.indent[] = ' ';
+        this.string.length = 0;
+        this.string_serializer.serializeStructArray(this.string, name, array, serialize_element);
+        Trace.format("{}", this.string).flush();
     }
 }
 
