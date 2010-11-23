4f1309c50fac2c3987137d0de6488ca03ce019e5
Gavin Norman <gavin.norman@sociomantic.com>
2010-11-23 12:00:05 +0000

    + error handling & message formatting

--- text/xml/c/LibXml2.d
+++ text/xml/c/LibXml2.d
@@ -14,6 +14,24 @@ module ocean.text.xml.c.LibXml2;
 
 
 
+/*******************************************************************************
+
+    Imports
+
+*******************************************************************************/
+
+private import ocean.core.Array;
+
+private import tango.stdc.string;
+
+
+
+/*******************************************************************************
+
+    D bindings for C library
+
+*******************************************************************************/
+
 extern ( C )
 {
     /***************************************************************************
@@ -34,6 +52,24 @@ extern ( C )
     struct xmlDoc;
 
     public alias xmlDoc* xmlDocPtr;
+
+
+    /***************************************************************************
+
+        Initialises the xml parser
+    
+    ***************************************************************************/
+    
+    void xmlInitParser ( );
+    
+    
+    /***************************************************************************
+    
+        Cleans up any global xml parser allocations.
+    
+    ***************************************************************************/
+    
+    void xmlCleanupParser ( );
 
 
     /***************************************************************************
@@ -71,13 +107,83 @@ extern ( C )
 
     void xmlFreeDoc ( xmlDocPtr cur );
 
-
+    
     /***************************************************************************
 
-        Cleans up any global xml parser allocations.
+        Xml error level enum
     
     ***************************************************************************/
 
-    void xmlCleanupParser ( );
+    enum xmlErrorLevel
+    {
+        XML_ERR_NONE = 0,
+        XML_ERR_WARNING = 1,    // A simple warning
+        XML_ERR_ERROR = 2,      // A recoverable error
+        XML_ERR_FATAL = 3       // A fatal error
+    }
+
+
+    /***************************************************************************
+
+        Xml error struct & pointer type
+    
+    ***************************************************************************/
+
+    struct xmlError
+    {
+        int domain;             // What part of the library raised this error
+        int code;               // The error code, e.g. an xmlParserError
+        char* message;          // human-readable informative error messag
+        xmlErrorLevel level;    // how consequent is the error
+        char* file;             // the filename
+        int line;               // the line number if available
+        char* str1;             // extra string information
+        char* str2;             // extra string information
+        char* str3;             // extra string information
+        int int1;               // extra number information
+        int int2;               // column number of the error or 0 if N/A
+        void* ctxt;             // the parser context if available
+        void* node;             // the node in the tree
+    }
+
+    public alias xmlError* xmlErrorPtr;
+
+
+    /***************************************************************************
+
+        Gets a pointer to the error struct for the last command
+    
+    ***************************************************************************/
+
+    xmlErrorPtr xmlGetLastError ( );
+}
+
+
+
+/*******************************************************************************
+
+    Helper function to format a D string with an error report.
+
+    Params:
+        err = libxml2 error structure
+        string = output string
+
+*******************************************************************************/
+
+public void formatXmlErrorString ( xmlErrorPtr err, ref char[] string )
+{
+    char[] dstr ( char* cstr )
+    {
+        if ( cstr )
+        {
+            return cstr[0..strlen(cstr)];
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    string.concat("Xml parsing error: ", dstr(err.message));
 }
 
