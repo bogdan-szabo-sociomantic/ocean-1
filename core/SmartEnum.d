03b0e5cf0e3f54709144e4de031eb552e365adc0
Gavin Norman <gavin.norman@sociomantic.com>
2011-04-05 14:57:03 +0000

    * Supports byte / ubyte enum base types properly now. + Added static opIndex methods, same behaviour as associative arrays. + Added AutoSmartEnum mixin template, which generates a SmartEnum with automatic numbering.

--- core/SmartEnum.d
+++ core/SmartEnum.d
@@ -11,16 +11,24 @@ module ocean.core.SmartEnum;
  * Everything in the class is statically defined and can be used without an
  * instance.
  * 
- * TODO
- * 1. AutoSmartEnum, which doesn't need numbers to be given. (Can work the same
- *    as AutoStringEnum.)
  */
 
 
 
 public import ocean.core.TwoWayMap;
+public import ocean.core.Exception;
 
 private import tango.core.Traits;
+private import tango.core.Tuple;
+
+
+
+// Just for the sake of being able to tell if a class is a SmartEnum or not,
+// using is(T : ISmartEnum).
+
+public abstract class ISmartEnum
+{
+}
 
 
 
@@ -34,9 +42,9 @@ public struct SmartEnumValue ( T )
 
 
 
-// This is template mixed-in to the SmartEnum class, right at the bottom of the module.
+// This template is mixed-in to the SmartEnum class, right at the bottom of the module.
 // (The rest is all string mixins.)
-private template SmartEnumMethods ( BaseType )
+private template SmartEnumMethods ( char[] Name, BaseType )
 {
     static public char[]* description ( BaseType code )
     {
@@ -52,6 +60,21 @@ private template SmartEnumMethods ( BaseType )
 
     public alias code opIn_r;
 
+    
+    static public BaseType opIndex ( char[] description )
+    {
+        auto code = description in map;
+        assertEx(code, description ~ " not found in SmartEnum " ~ Name);
+        return *code;
+    }
+    
+    static public char[] opIndex ( BaseType code )
+    {
+        auto description = code in map;
+        assertEx(description, "code not found in SmartEnum " ~ Name);
+        return *description;
+    }
+    
     static public size_t* indexOf ( BaseType code )
     {
         return map.indexOf(code);
@@ -94,15 +117,32 @@ private template SmartEnumMethods ( BaseType )
 }
 
 
+private char[] CTFE_Int2String ( T ) ( T num )
+{
+    static if ( is(T == ubyte) )
+    {
+        return ctfe_i2a(cast(uint)num);
+    }
+    else static if ( is(T == byte) )
+    {
+        return ctfe_i2a(cast(int)num);
+    }
+    else
+    {
+        return ctfe_i2a(num);
+    }
+}
+
+
 private template EnumValuesList ( T ... )
 {
     static if ( T.length == 1 )
     {
-        const char[] EnumValuesList = T[0].name ~ "=" ~ ctfe_i2a(T[0].value);
+        const char[] EnumValuesList = T[0].name ~ "=" ~ CTFE_Int2String(T[0].value);
     }
     else
     {
-        const char[] EnumValuesList = T[0].name ~ "=" ~ ctfe_i2a(T[0].value) ~ "," ~ EnumValuesList!(T[1..$]);
+        const char[] EnumValuesList = T[0].name ~ "=" ~ CTFE_Int2String(T[0].value) ~ "," ~ EnumValuesList!(T[1..$]);
     }
 }
 
@@ -194,16 +234,16 @@ private template DeclareConstants ( T ... )
 {
     const char[] DeclareConstants =
         "static const length = " ~ ctfe_i2a(T.length) ~ "; " ~
-        "static const min = " ~ ctfe_i2a(MinValue!(T)) ~ "; " ~
-        "static const max = " ~ ctfe_i2a(MaxValue!(T)) ~ "; " ~
+        "static const min = " ~ CTFE_Int2String(MinValue!(T)) ~ "; " ~
+        "static const max = " ~ CTFE_Int2String(MaxValue!(T)) ~ "; " ~
         "static const min_descr_length = " ~ ctfe_i2a(ShortestName!(T)) ~ "; " ~
         "static const max_descr_length = " ~ ctfe_i2a(LongestName!(T)) ~ "; ";
 }
 
 
-private template MixinMethods ( T ... )
+private template MixinMethods ( char[] Name, T ... )
 {
-    const char[] MixinMethods = "mixin SmartEnumMethods!(" ~ T[0].BaseType.stringof ~ ");";
+    const char[] MixinMethods = `mixin SmartEnumMethods!("` ~ Name ~ `", ` ~ T[0].BaseType.stringof ~ ");";
 }
 
 
@@ -211,12 +251,36 @@ public template SmartEnum ( char[] Name, T ... )
 {
     static if ( T.length > 0 )
     {
-        const char[] SmartEnum = "class " ~ Name ~ " { " ~ DeclareEnum!(T) ~ DeclareMap!(T) ~ DeclareConstants!(T) ~
-            StaticThis!(T) ~ MixinMethods!(T) ~ "}";
+        const char[] SmartEnum = "class " ~ Name ~ " : ISmartEnum { " ~ DeclareEnum!(T) ~ DeclareMap!(T) ~ DeclareConstants!(T) ~
+            StaticThis!(T) ~ MixinMethods!(Name, T) ~ "}";
     }
     else
     {
         static assert(false, "Cannot create a SmartEnum with no entries!");
     }
+}
+
+
+
+
+
+template CreateCodes ( BaseType, uint i, Strings ... )
+{
+    static if ( Strings.length == 1 )
+    {
+        alias Tuple!(SmartEnumValue!(BaseType)(Strings[0], i)) CreateCodes; 
+    }
+    else
+    {
+        alias Tuple!(SmartEnumValue!(BaseType)(Strings[0], i), CreateCodes!(BaseType, i + 1, Strings[1 .. $])) CreateCodes;
+    }
+}
+
+
+public template AutoSmartEnum ( char[] Name, BaseType, Strings ... )
+{
+    static assert ( is(typeof(Strings[0]) : char[]), "AutoSmartEnum - please only give char[]s as template parameters");
+
+    const char[] AutoSmartEnum = SmartEnum!(Name, CreateCodes!(BaseType, 0, Strings));
 }
 
