eeec4af30674ef34f435f63ffc2deacab155a18d
Gavin Norman <gavin.norman@sociomantic.com>
2011-05-19 12:50:22 +0000

    + EBTreeMap with fast key lookup -- extends basic EBTreeMap class. + Unittest. * Bug fixes.

--- db/ebtree/EBTreeMap.d
+++ db/ebtree/EBTreeMap.d
@@ -59,7 +59,7 @@
         // Get the highest value in the map
         auto lowest = map.last;
     
-        // Iterate over all keys and values in the map
+        // Iterate over all keys and values in the map, in key order
         foreach ( key, value; map )
         {
             // do something
@@ -82,9 +82,13 @@ module ocean.db.ebtree.EBTreeMap;
 
 *******************************************************************************/
 
-private import tango.stdc.time : time_t;
+private import ocean.core.ArrayMap;
 
 private import ocean.db.ebtree.EBTree;
+
+private import tango.stdc.time : time_t;
+
+debug private import tango.util.log.Trace;
 
 
 
@@ -100,10 +104,14 @@ private import ocean.db.ebtree.EBTree;
     Template params:
         Key = type of keys stored in map (must be 32-bits)
         Value = type of values stored in map (must be 32-bits)
+        KeyUnique = flag telling whether it's possible to store multiple entries
+            in the map with the same key (the default). Note that for unique
+            maps, a lookup operation is required each time an entry is added, to
+            avoid adding duplicates.
 
 *******************************************************************************/
 
-class EBTreeMap ( Key, Value )
+class EBTreeMap ( Key, Value, bool KeyUnique = false )
 {
     /***************************************************************************
 
@@ -133,28 +141,45 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        Struct defining a single entry in the map -- a key and a value.
+        Struct defining a single entry in the map -- a key and a value. The key
+        is always placed as the most significant bytes, so the tree is sorted in
+        key order.
 
     ***************************************************************************/
 
-    private struct NodeData
+    version ( BigEndian )
     {
-        Key key;
-        Value value;
+        private struct MappingData
+        {
+            Key key;
+            Value value;
+        }
+    }
+    else version ( LittleEndian )
+    {
+        private struct MappingData
+        {
+            Value value;
+            Key key;
+        }
+    }
+    else
+    {
+        static assert(false, This.stringof ~ ": endianness version not found, cannot safely use this template");
     }
 
 
     /***************************************************************************
 
         Union for converting between the actual values which are stored in the
-        EBTree (ulongs -- 64-bit integers) and the NodeData struct declared
+        EBTree (ulongs -- 64-bit integers) and the MappingData struct declared
         above.
 
     ***************************************************************************/
 
     private union NodeUnion
     {
-        NodeData data;
+        MappingData data;
         ulong integer;
     }
 
@@ -175,7 +200,7 @@ class EBTreeMap ( Key, Value )
 
     ***************************************************************************/
 
-    public alias Tree.Node Node;
+    public alias Tree.Node Mapping;
 
 
     /***************************************************************************
@@ -194,37 +219,131 @@ class EBTreeMap ( Key, Value )
 
         Adds a mapping.
 
+        Note: this method is also aliased as put().
+
         Params:
             key = key
             value = value
 
         Returns:
-            pointer to new node in tree
+            pointer to new mapping in tree
 
     ***************************************************************************/
 
-    public Node* add ( Key key, Value value )
+    public Mapping* add ( Key key, Value value )
     {
         NodeUnion item;
         item.data.key = key;
         item.data.value = value;
 
-        return this.tree.add(item.integer);
+        static if ( KeyUnique )
+        {
+            auto mapping = this.lookupMapping(key);
+            if ( mapping is null )
+            {
+                return this.tree.add(item.integer);
+            }
+            else
+            {
+                return this.update(mapping, value);
+            }
+        }
+        else
+        {
+            return this.tree.add(item.integer);
+        }
+    }
+
+    public alias add put;
+
+
+    /***************************************************************************
+
+        Updates a mapping's value, given a mapping.
+
+        Params:
+            mapping = mapping to update
+            value = new value
+
+        Returns:
+            pointer to updated mapping in tree
+    
+    ***************************************************************************/
+
+    public Mapping* update ( Mapping* mapping, Value value )
+    in
+    {
+        assert(mapping !is null, This.stringof ~ ".update: cannot update a null mapping");
+    }
+    body
+    {
+        NodeUnion item;
+        item.integer = mapping.key;
+        item.data.value = value;
+
+        mapping.key = item.integer;
+
+        return mapping;
     }
 
 
     /***************************************************************************
 
-        Removes a node from the tree.
+        Updates a mapping's key and value, given a mapping. If the mapping's
+        key changes, it must be removed from and re-inserted into the tree to
+        maintain the tree's sort order.
+
+        Params:
+            mapping = mapping to update
+            key = new key
+            value = new value
+
+        Returns:
+            pointer to updated mapping in tree
+
+    ***************************************************************************/
+
+    public Mapping* update ( Mapping* mapping, Key key, Value value )
+    in
+    {
+        assert(mapping !is null, This.stringof ~ ".update: cannot update a null mapping");
+    }
+    body
+    {
+        NodeUnion item;
+        item.integer = mapping.key;
+
+        if ( key != item.data.key ) // Key has changed, map needs re-sorting
+        {
+            // Remove old mapping and add new one
+            this.remove(mapping);
+
+            return this.add(key, value);
+        }
+        else // Key same, don't need re-sort
+        {
+            // Update existing mapping
+            item.data.value = value;
+
+            mapping.key = item.integer;
+
+            return mapping;
+        }
+    }
+
+
+    /***************************************************************************
+
+        Removes a mapping from the tree.
     
         Params:
-            node = pointer to node to remove
+            mapping = pointer to mapping to remove
     
     ***************************************************************************/
 
-    public void remove ( Node* node )
+    public void remove ( Mapping* mapping )
     {
-        this.tree.remove(node);
+        this.tree.remove(mapping);
     }
 
 
@@ -255,25 +374,25 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        Gets the mapped value corresponding to a node in the tree.
+        Gets the mapped value corresponding to a mapping in the tree.
 
         Params:
-            node = pointer to node
+            mapping = pointer to mapping
     
         Returns:
-            value of tree node
+            value of mapping
 
     ***************************************************************************/
 
-    public Value getValue ( Node* node )
+    public Value mappingValue ( Mapping* mapping )
     in
     {
-        assert(node !is null, This.stringof ~ ".getValue: cannot get a value from a null node");
+        assert(mapping !is null, This.stringof ~ ".getValue: cannot get a value from a null mapping");
     }
     body
     {
         NodeUnion item;
-        item.integer = node.key;
+        item.integer = mapping.key;
 
         return item.data.value;
     }
@@ -281,27 +400,103 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        Gets the mapped key corresponding to a node in the tree.
+        Gets the mapped key corresponding to a mapping in the tree.
 
         Params:
-            node = pointer to node
+            mapping = pointer to mapping
 
         Returns:
-            key of tree node
+            key of mapping
 
     ***************************************************************************/
 
-    public Value getKey ( Node* node )
+    public Value mappingKey ( Mapping* mapping )
     in
     {
-        assert(node !is null, This.stringof ~ ".getKey: cannot get a key from a null node");
+        assert(mapping !is null, This.stringof ~ ".getKey: cannot get a key from a null mapping");
     }
     body
     {
         NodeUnion item;
-        item.integer = node.key;
+        item.integer = mapping.key;
     
+        return item.data.key;
+    }
+
+
+    /***************************************************************************
+
+        Gets the value mapped by the the specified key. Note that it is possible
+        for multiple mappings to have the same key, in this case only the first
+        value is returned.
+
+        Note: this lookup is not especially efficient, as it involves a tree
+        search.
+
+        Params:
+            key = key to look up
+
+        Returns:
+            value mapped to by key
+
+        Throws:
+            exception if key not found in map
+
+    ***************************************************************************/
+
+    public Value lookup ( Key key )
+    {
+        auto mapping = this.lookupMapping(key);
+        if ( mapping is null )
+        {
+            throw new Exception(This.stringof ~ ".lookup: no mapping found");
+        }
+
+        NodeUnion item;
+        item.integer = mapping.key;
         return item.data.value;
+    }
+
+
+    /***************************************************************************
+
+        Gets the mapping for the specified key. Note that it is possible for
+        multiple mappings to have the same key, in this case only the first is
+        returned. Further mappings can be fetched using the mapping's next()
+        method.
+
+        Note: this lookup is not especially efficient, as it involves a tree
+        search.
+
+        Params:
+            key = key to look up
+
+        Returns:
+            mapping containing key, can be null if key not found
+
+    ***************************************************************************/
+
+    public Mapping* lookupMapping ( Key key )
+    {
+        NodeUnion item;
+        item.data.key = key;
+        item.data.value = 0;
+
+        auto mapping = this.tree.firstNodeGreaterEqual(item.integer);
+        if ( mapping is null )
+        {
+            return null;
+        }
+
+        item.integer = mapping.key;
+        if ( item.data.key == key )
+        {
+            return mapping;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 
@@ -359,14 +554,14 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        Gets the first node in the tree (which contains the lowest key).
+        Gets the first mapping in the tree (which contains the lowest key).
 
         Returns:
-            first node in tree, null if tree is empty
+            first mapping in tree, null if tree is empty
 
     ***************************************************************************/
 
-    public Node* firstNode ( )
+    public Mapping* firstMapping ( )
     {
         return this.tree.firstNode;
     }
@@ -374,14 +569,14 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        Gets the last node in the tree (which contains the highest key).
+        Gets the last mapping in the tree (which contains the highest key).
 
         Returns:
-            last node in tree, null if tree is empty
+            last mapping in tree, null if tree is empty
 
     ***************************************************************************/
 
-    public Node* lastNode ( )
+    public Mapping* lastMapping ( )
     {
         return this.tree.lastNode;
     }
@@ -461,12 +656,12 @@ class EBTreeMap ( Key, Value )
 
     /***************************************************************************
 
-        foreach iterator over all nodes in the tree and their corresponding
+        foreach iterator over all mappings in the tree and their corresponding
         keys & values, in key order.
 
     ***************************************************************************/
 
-    public int opApply ( int delegate ( ref Node* node, ref Key key, ref Value value ) dg )
+    public int opApply ( int delegate ( ref Mapping* node, ref Key key, ref Value value ) dg )
     {
         int ret;
 
@@ -484,5 +679,200 @@ class EBTreeMap ( Key, Value )
     }
 
     // TODO: lessEqual / greaterEqual iterators, if needed (see EBTree)
+}
+
+
+
+/*******************************************************************************
+
+    EBTree ordered map class template with fast key lookup. Extends EBTreeMap
+    and keeps an internal array map from keys to tree nodes, enabling fast
+    lookup of keys without requiring a tree search.
+
+    Template params:
+        Key = type of keys stored in map (must be 32-bits)
+        Value = type of values stored in map (must be 32-bits)
+
+*******************************************************************************/
+
+class EBTreeMapFastLookup ( Key, Value ) : EBTreeMap!(Key, Value, true)
+{
+    /***************************************************************************
+
+        Mapping from keys to tree nodes, for fast node lookup
+
+    ***************************************************************************/
+
+    private alias ArrayMap!(Mapping*, Key) KeyToMapping;
+
+    private KeyToMapping key_to_mapping;
+
+
+    /***************************************************************************
+
+        Constructor.
+
+    ***************************************************************************/
+
+    public this ( )
+    {
+        super();
+
+        this.key_to_mapping = new KeyToMapping;
+    }
+
+
+    /***************************************************************************
+
+        Adds a mapping.
+    
+        Note: this method is also aliased as put().
+    
+        Params:
+            key = key
+            value = value
+    
+        Returns:
+            pointer to new mapping in tree
+    
+    ***************************************************************************/
+
+    override public Mapping* add ( Key key, Value value )
+    {
+        auto mapping = super.add(key, value);
+
+        this.key_to_mapping.put(key, mapping);
+
+        return mapping;
+    }
+
+
+    /***************************************************************************
+
+        Removes a mapping from the tree.
+
+        Params:
+            mapping = pointer to mapping to remove
+
+    ***************************************************************************/
+
+    override public void remove ( Mapping* mapping )
+    {
+        this.key_to_mapping.remove(super.mappingKey(mapping));
+
+        super.tree.remove(mapping);
+    }
+
+
+    /***************************************************************************
+
+        Removes all mappings.
+    
+    ***************************************************************************/
+    
+    override public void clear ( )
+    {
+        this.key_to_mapping.clear;
+
+        super.clear;
+    }
+
+
+    /***************************************************************************
+
+        Gets the mapping for the specified key.
+
+        Params:
+            key = key to look up
+
+        Returns:
+            mapping containing key, can be null if key not found
+
+    ***************************************************************************/
+
+    override public Mapping* lookupMapping ( Key key )
+    {
+        auto mapping = key in this.key_to_mapping;
+        return (mapping is null) ? null : *mapping;
+    }
+}
+
+
+
+/*******************************************************************************
+
+    Unittest
+
+*******************************************************************************/
+
+debug ( OceanUnitTest )
+{
+    void testUniqueMap ( Map ) ( )
+    {
+        // Create map
+        scope unique_map = new EBTreeMapFastLookup!(hash_t, uint);
+        assert(unique_map.length == 0);
+    
+        // Add a mapping
+        unique_map.put(1, 23);
+        assert(unique_map.length == 1);
+        assert(unique_map.first == 23);
+        assert(unique_map.last == 23);
+    
+        // Replace the mapping (duplicate key)
+        unique_map.put(1, 25);
+        assert(unique_map.length == 1);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 25);
+    
+        // Add a mapping
+        unique_map.put(2, 26);
+        assert(unique_map.length == 2);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 26);
+    
+        // Add a mapping
+        unique_map.put(3, 27);
+        assert(unique_map.length == 3);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 27);
+    
+        // Update last mapping, value change
+        unique_map.update(unique_map.lastMapping, 28);
+        assert(unique_map.length == 3);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 28);
+    
+        // Update last mapping, key change, switched to front
+        unique_map.update(unique_map.lastMapping, 0, 28);
+        assert(unique_map.length == 3);
+        assert(unique_map.first == 28);
+        assert(unique_map.last == 26);
+    
+        // Remove first mapping
+        unique_map.remove(unique_map.firstMapping);
+        assert(unique_map.length == 2);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 26);
+    
+        // Remove last mapping
+        unique_map.remove(unique_map.lastMapping);
+        assert(unique_map.length == 1);
+        assert(unique_map.first == 25);
+        assert(unique_map.last == 25);
+    
+        // Clear map
+        unique_map.clear;
+        assert(unique_map.length == 0);
+    }
+    
+    unittest
+    {
+        // Test normal map
+        testUniqueMap!(EBTreeMap!(hash_t, uint, true));
+    
+        // Test fast-lookup map
+        testUniqueMap!(EBTreeMapFastLookup!(hash_t, uint));
+    }
 }
 
