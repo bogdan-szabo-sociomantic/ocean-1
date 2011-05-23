05523adc44e545c5ab4478f46dc1fe36e13084b2
David Eckardt <david.eckardt@sociomantic.com>
2011-05-23 20:01:50 +0000

    * updated documentation

--- net/http2/consts/HttpMethod.d
+++ net/http2/consts/HttpMethod.d
@@ -1,4 +1,24 @@
+/******************************************************************************
+
+    HTTP method name constants and enumerator
+    
+    copyright:      Copyright (c) 2011 sociomantic labs. All rights reserved
+    
+    version:        May 2011: Initial release
+    
+    author:         David Eckardt
+    
+    TODO: add support for extension methods (when needed)
+    
+ ******************************************************************************/
+
 module ocean.net.http2.consts.HttpMethod;
+
+/******************************************************************************
+
+    HTTP method enumerator
+
+ ******************************************************************************/
 
 enum HttpMethod : ubyte
 {
@@ -13,8 +33,20 @@ enum HttpMethod : ubyte
     Options
 }
 
+/******************************************************************************
+
+    HTTP method name string constants and enumerator value association
+
+ ******************************************************************************/
+
 struct HttpMethodNames
 {
+    /**************************************************************************
+
+        HTTP method name string constants
+    
+     **************************************************************************/
+
     const char[][HttpMethod.max + 1] List =
     [
         HttpMethod.Undefined:  "",
@@ -28,8 +60,28 @@ struct HttpMethodNames
         HttpMethod.Options:    "OPTIONS"
     ];
     
+    /**************************************************************************
+
+        HTTP method enumerator value by name string 
+    
+     **************************************************************************/
+
     private static HttpMethod[char[]] methods_by_name;
     
+    /**************************************************************************
+
+        Obtains the HTTP method enumerator value by name string. Does not throw
+        an exception.
+    
+        Params:
+            name = HTTP method name string
+            
+         Returns:
+             HTTP method enumerator value corresponding to name string or
+             HttpMethod.Undefined if the name string is unknown.
+    
+     **************************************************************************/
+
     static HttpMethod opIndex ( char[] name )
     {
         HttpMethod* method = name? name in this.methods_by_name : null;
@@ -37,11 +89,31 @@ struct HttpMethodNames
         return method? *method : HttpMethod.init;
     }
     
+    /**************************************************************************
+
+        Obtains the HTTP method name string by enumerator value. Does not throw
+        an exception.
+    
+        Params:
+            method = HTTP method enumerator value
+            
+         Returns:
+             HTTP method name string corresponding to name method or null on
+             invalid value.
+    
+     **************************************************************************/
+
     static char[] opIndex ( HttpMethod method )
     {
         return (method <= method.max)? this.List[method] : null;
     }
     
+    /**************************************************************************
+
+        Static constructor; populates the association map
+    
+     **************************************************************************/
+
     static this ( )
     {
         foreach (method, name; this.List)
