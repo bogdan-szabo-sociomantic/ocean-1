f095283b7ec71aa57df6c8dd6b56322851b14a74
David Eckardt <david.eckardt@sociomantic.com>
2011-05-23 19:48:57 +0000

    * HeaderFieldNames: constants defined in the module instead of import from Tango because Tango's constants have a tailing ':'

--- net/http2/consts/HeaderFieldNames.d
+++ net/http2/consts/HeaderFieldNames.d
@@ -1,9 +1,5 @@
 module ocean.net.http2.consts.HeaderFieldNames;
 
-private import tango.net.http.HttpConst;
-
-private import tango.stdc.string: memchr;
-
 struct HeaderFieldNames
 {
     /**************************************************************************
@@ -18,67 +14,31 @@ struct HeaderFieldNames
     
     struct Request
     {
-        char[] CacheControl,
-               Connection,
-               Date,
-               Pragma,
-               Trailer,
-               TransferEncoding,
-               Upgrade,
-               Via,
-               Warning,
+        char[] CacheControl,        Connection,         Date,
+               Pragma,              Trailer,            TransferEncoding,
+               Upgrade,             Via,                Warning,
                 
-               Accept,
-               AcceptCharset,
-               AcceptEncoding,
-               AcceptLanguage,
-               Authorization,
-               Expect,
-               From,
-               Host,
-               IfMatch,
-               IfModifiedSince,
-               IfNoneMatch,
-               IfRange,
-               IfUnmodifiedSince,
-               MaxForwards,
-               ProxyAuthorization,
-               Range,
-               Referer,
-               TE,
+               Accept,              AcceptCharset,      AcceptEncoding,
+               AcceptLanguage,      Authorization,      Expect,
+               From,                Host,               IfMatch,
+               IfModifiedSince,     IfNoneMatch,        IfRange,
+               IfUnmodifiedSince,   MaxForwards,        ProxyAuthorization,
+               Range,               Referer,            TE,
                UserAgent;
         
         const typeof (*this) Names =
         {
-             HttpHeader.CacheControl.value,
-             HttpHeader.Connection.value,
-             HttpHeader.Date.value,
-             HttpHeader.Pragma.value,
-             HttpHeader.Trailer.value,
-             HttpHeader.TransferEncoding.value,
-             HttpHeader.Upgrade.value,
-             "Via:",                                // missing in tango's HttpHeader
-             HttpHeader.Warning.value,
-           
-             HttpHeader.Accept.value,
-             HttpHeader.AcceptCharset.value,
-             HttpHeader.AcceptEncoding.value,
-             HttpHeader.AcceptLanguage.value,
-             HttpHeader.Authorization.value,
-             HttpHeader.Expect.value,
-             HttpHeader.From.value,
-             HttpHeader.Host.value,
-             HttpHeader.IfMatch.value,
-             HttpHeader.IfModifiedSince.value,
-             HttpHeader.IfNoneMatch.value,
-             HttpHeader.IfRange.value,
-             HttpHeader.IfUnmodifiedSince.value,
-             HttpHeader.MaxForwards.value,
-             "Proxy-Authorization:",            // missing in tango's HttpHeader
-             HttpHeader.Range.value,
-             HttpHeader.Referrer.value,
-             HttpHeader.TE.value,
-             HttpHeader.UserAgent.value
+            "Cache-Control",        "Connection",       "Date",
+            "Pragma",               "Trailer",          "Transfer-Encoding",
+            "Upgrade",              "Via",              "Warning",
+            
+            "Accept",               "Accept-Charset",   "Accept-Encoding",
+            "Accept-Language",      "Authorization",    "Expect",
+            "From",                 "Host",             "If-Match",
+            "If-Modified-Since",    "If-None-Match",    "If-Range",
+            "If-Unmodified-Since",  "Max-Forwards",     "Proxy-Authorization",
+            "Range",                "Referer",          "TE",
+            "User-Agent"
         };
         
         mixin NameList!();
@@ -95,74 +55,38 @@ struct HeaderFieldNames
         Entity header fields: Allow .. LastModified
         @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.1
     
-      **************************************************************************/             
+     **************************************************************************/             
 
     struct Response
     {
-        char[] CacheControl,
-               Connection,
-               Date,
-               Pragma,
-               Trailer,
-               TransferEncoding,
-               Upgrade,
-               Via,
-               Warning,
+        char[] CacheControl,        Connection,         Date,
+               Pragma,              Trailer,            TransferEncoding,
+               Upgrade,             Via,                Warning,
                
-               AcceptRanges,
-               Age,
-               ETag,
-               Location,
-               ProxyAuthenticate,
-               RetryAfter,
-               Server,
-               Vary,
-               WwwAuthenticate,
+               AcceptRanges,        Age,                ETag,
+               Location,            ProxyAuthenticate,  RetryAfter,
+               Server,              Vary,               WwwAuthenticate,
                
-               Allow,
-               ContentEncoding,
-               ContentLanguage,
-               ContentLength,
-               ContentLocation,
-               ContentMD5,
-               ContentRange,
-               ContentType,
-               Expires,
+               Allow,               ContentEncoding,    ContentLanguage,
+               ContentLength,       ContentLocation,    ContentMD5,
+               ContentRange,        ContentType,        Expires,
                LastModified;
                
         
         const typeof (*this) Names =
         {
-            HttpHeader.CacheControl.value,
-            HttpHeader.Connection.value,
-            HttpHeader.Date.value,
-            HttpHeader.Pragma.value,
-            HttpHeader.Trailer.value,
-            HttpHeader.TransferEncoding.value,
-            HttpHeader.Upgrade.value,
-            "Via:",                             // missing in tango's HttpHeader
-            HttpHeader.Warning.value,
-               
-            HttpHeader.AcceptRanges.value,
-            HttpHeader.Age.value,
-            HttpHeader.ETag.value,
-            HttpHeader.Location.value,
-            HttpHeader.ProxyAuthenticate.value,
-            HttpHeader.RetryAfter.value,
-            HttpHeader.Server.value,
-            HttpHeader.Vary.value,
-            HttpHeader.WwwAuthenticate.value,
+            "Cache-Control",        "Connection",       "Date",
+            "Pragma",               "Trailer",          "Transfer-Encoding",
+            "Upgrade",              "Via",              "Warning",
             
-            HttpHeader.Allow.value,
-            HttpHeader.ContentEncoding.value,
-            HttpHeader.ContentLanguage.value,
-            HttpHeader.ContentLength.value,
-            HttpHeader.ContentLocation.value,
-            "Content-MD5",                      // missing in tango's HttpHeader
-            HttpHeader.ContentRange.value,
-            HttpHeader.ContentType.value,
-            HttpHeader.Expires.value,
-            HttpHeader.LastModified.value
+            "Accept-Ranges",        "Age",              "ETag",
+            "Location",             "Proxy-Authenticate","Retry-After",
+            "Server",               "Vary",             "WWW-Authenticate",
+            
+            "Allow",                "Content-Encoding", "Content-Language",
+            "Content-Length",       "Content-Location", "Content-MD5",
+            "Content-Range",        "Content-Type",     "Expires",
+            "Last-Modified"
         };
         
         mixin NameList!();
@@ -173,65 +97,15 @@ struct HeaderFieldNames
         static typeof (this.Names.tupleof)[0][(typeof (this.Names.tupleof)).length] NameList; 
         
         static this ( )
-        out
         {
-            char[] name = containsColon(this.NameList);
-            
-            assert (!name, "\"" ~ name ~ "\" - invalid HTTP header name (contains ':')");
-        }
-        body
-        {
-            alias typeof (this.Names.tupleof)[0] T;
-            
             foreach (i, name; this.Names.tupleof)
             {
-                static assert (is (typeof (name) == T));
+                assert (name.length, typeof (*this).stringof ~
+                                     this.Names.tupleof[i].stringof[this.Names.stringof.length .. $] ~
+                                     " is empty");
                 
-                this.NameList[i] = stripTailingColon(name);
+                this.NameList[i] = name;
             }
         }
-    }
-    
-    /**************************************************************************
-
-        Scans key for ':'.
-        
-        Params:
-            key = key to scan for ':'
-            
-        Returns:
-            true if key contains ':' or false otherwise
-        
-     **************************************************************************/
-
-    public static char[] containsColon ( char[][] names ... )
-    {
-        foreach (name; names)
-        {
-            if (memchr(name.ptr, ':', name.length))
-            {
-                return name;
-            }
-        }
-        
-        return null;
-    }
-    
-    /**************************************************************************
-
-        Strips the tailing ':' from key if present. This is the only place where
-        a ':' may appear in key.
-        
-        Params:
-            key = key to strip tailing ':'
-            
-        Returns:
-            result (slices key)
-        
-     **************************************************************************/
-    
-    private static char[] stripTailingColon ( char[] key )
-    {
-        return key.length? key[0 .. $ - (key[$ - 1] == ':')] : key;
     }
 }
