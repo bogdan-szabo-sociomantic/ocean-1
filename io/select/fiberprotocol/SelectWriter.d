358c9a46d8c7bb1118e709f9a8c7a979211e1479
David Eckardt <david.eckardt@sociomantic.com>
2011-05-23 19:47:00 +0000

    * fiber handling moved to ISelectProtocol * ISelectProtocol/SelectReader/SelectWriter: several error detection bugs fixed + documentation

--- io/select/fiberprotocol/SelectWriter.d
+++ io/select/fiberprotocol/SelectWriter.d
@@ -1,3 +1,18 @@
+/******************************************************************************
+
+    Fiber/coroutine based non-blocking output select client base class
+
+    copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
+
+    version:        December 2010: Initial release
+
+    authors:        David Eckardt, Gavin Norman
+
+    Base class for a non-blocking output select client using a fiber/coroutine to
+    suspend operation while waiting for the read event and resume on that event.
+    
+ ******************************************************************************/
+
 module ocean.io.select.fiberprotocol.SelectWriter;
 
 private import ocean.io.select.fiberprotocol.model.ISelectProtocol;
@@ -6,37 +21,111 @@ private import ocean.io.select.model.ISelectClient;
 
 private import tango.io.model.IConduit: OutputStream;
 
-debug private import tango.util.log.Trace;
+private import tango.stdc.errno: errno;
 
 class SelectWriter : ISelectProtocol
 {
+    /**************************************************************************
+
+        Constructor
+        
+        Params:
+            conduit = output conduit (must be an OutputStream)
+            fiber   = output reading fiber
+            
+     **************************************************************************/
+
     this ( ISelectable conduit, Fiber fiber )
+    in
+    {
+        assert (conduit !is null);
+        assert ((cast (OutputStream) conduit) !is null);
+    }
+    body
     {
         super(conduit, fiber);
     }
 
+    /**************************************************************************
+
+        Mandated by the ISelectClient interface
+        
+        Returns:
+            I/O events to register the conduit of this instance for
+    
+     **************************************************************************/
+    
     Event events ( )
     {
         return Event.Write;
     }
     
-    void write ( void[] data )
+    /**************************************************************************
+
+        Writes data to the output conduit. Whenever the output conduit is not
+        ready for writing, the output writing fiber is suspended and continues
+        writing on resume.
+        
+        Returns:
+            this instance
+            
+        Throws:
+            IOException on end-of-flow condition:
+                - IOWarning if neither error is reported by errno nor socket
+                  error
+                - IOError if an error is reported by errno or socket error
+    
+     **************************************************************************/
+
+    typeof (this) send ( void[] data )
     {
-        for (bool more = this.send(data); more; more = this.send(data))
-        {
-            this.fiber.cede();
-        }
+        super.repeat(this.send_(data) != 0);
+        
+        return this;
     }
 
-    protected bool send ( void[] data )
+    /**************************************************************************
+
+        Attempts to write data to the output conduit. The output conduit may or
+        may not write all elements of data.
+        
+        Returns:
+            Number of elements (bytes) of the remaining amount of data; that is,
+            if the return value is x, data[0 .. $ - x] has been written. 0
+            indicates that all data has been written (or data is an empty
+            array).
+            
+        Throws:
+            IOException on end-of-flow condition:
+                - IOWarning if neither error is reported by errno nor socket
+                  error
+                - IOError if an error is reported by errno or socket error
+    
+     **************************************************************************/
+
+    private size_t send_ ( void[] data )
     {
         debug (Raw) Trace.formatln("<<< {:X2}", data);
-
-        size_t sent = (cast(OutputStream)conduit).write(data);
-
-        super.exception.assertEx(sent != OutputStream.Eof, "end of flow whilst writing", __FILE__, __LINE__);
         
-        return sent < data.length;
+        size_t sent = (cast (OutputStream) super.conduit).write(data);
+        
+        if (sent == OutputStream.Eof)
+        {
+            super.error_e.checkSocketError("write error", __FILE__, __LINE__);
+            
+            if (errno)
+            {
+                throw super.error_e(errno, "write error", __FILE__, __LINE__);
+            }
+            else
+            {
+                throw super.warning_e("end of flow whilst writing", __FILE__, __LINE__);
+            }
+        }
+        
+        assert (data.length >= sent);
+        
+        return data.length - sent;
     }
     
     /**************************************************************************
@@ -55,4 +144,3 @@ class SelectWriter : ISelectProtocol
         }
     }
 }
-
--- io/select/fiberprotocol/model/ISelectProtocol.d
+++ io/select/fiberprotocol/model/ISelectProtocol.d
@@ -1,60 +1,472 @@
+/******************************************************************************
+
+    Fiber/coroutine based non-blocking I/O select client base class
+
+    copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
+
+    version:        December 2010: Initial release
+
+    authors:        David Eckardt, Gavin Norman
+
+    Base class for a non-blocking I/O select client using a fiber/coroutine to
+    suspend operation while waiting for the I/O event and resume on that event.
+
+ ******************************************************************************/
+
 module ocean.io.select.fiberprotocol.model.ISelectProtocol;
+
+/******************************************************************************
+
+    Imports
+
+ ******************************************************************************/
 
 private import ocean.io.select.model.ISelectClient;
 
 private import ocean.core.Array: copy, concat;
 
-private import tango.core.Thread : Fiber;
-
 private import TangoException = tango.core.Exception: IOException;
+private import tango.net.device.Berkeley: socket_t, Berkeley;
 
 private import tango.stdc.string: strlen;
+private import tango.stdc.errno: errno;
 
-private import tango.stdc.errno;
+private import tango.io.Stdout;
 
-debug private import tango.util.log.Trace;
+private import tango.core.Thread : Fiber;
 
-private extern (C) char* strerror_r ( int errnum, char* buf, size_t buflen );
+/******************************************************************************
+
+    Obtains the system error message corresponding to errnum (reentrant/
+    thread-safe version of strerror()).
+    
+    Note: This is the GNU (not the POSIX) version of strerror_r().
+    
+    @see http://www.kernel.org/doc/man-pages/online/pages/man3/strerror.3.html
+    
+    "The GNU-specific strerror_r() returns a pointer to a string containing the
+     error message.  This may be either a pointer to a string that the function
+     stores in buf, or a pointer to some (immutable) static string (in which case
+     buf is unused).  If the function stores a string in buf, then at most buflen
+     bytes are stored (the string may be truncated if buflen is too small) and
+     the string always includes a terminating null byte."
+
+    Tries have shown that buffer may actually not be populated.
+    
+    Params:
+        errnum = error number
+        buffer = error message destination buffer (may or may not be populated)
+        buflen = destination buffer length
+    
+    Returns:
+        a NUL-terminated string containing the error message
+    
+ ******************************************************************************/
+
+private extern (C) char* strerror_r ( int errnum, char* buffer, size_t buflen );
+
+/******************************************************************************/
 
 abstract class ISelectProtocol : IAdvancedSelectClient
 {
-    alias .Fiber Fiber;
+    /**************************************************************************
+
+        Default I/O data buffer size
     
+     **************************************************************************/
+
     const buffer_size = 0x4000;
     
-    protected Fiber fiber;
+    /**************************************************************************
+
+        Event(s) reported to handle()
     
-    protected IOException exception;
+     **************************************************************************/
+
+    protected Event event;
     
+    /**************************************************************************
+
+        IOWarning exception instance 
+    
+     **************************************************************************/
+
+    protected IOWarning warning_e;
+    
+    /**************************************************************************
+
+        IOError exception instance 
+    
+     **************************************************************************/
+
+    protected IOError   error_e;
+    
+    /**************************************************************************
+
+        Fiber (may be shared across instances of this class)
+    
+     **************************************************************************/
+
+    private Fiber fiber;
+    
+    /**************************************************************************
+
+        Constructor
+        
+         Params:
+             conduit = I/O device
+             fiber   = fiber to use to suspend and resume operation
+    
+     **************************************************************************/
+
     this ( ISelectable conduit, Fiber fiber )
     {
         super(conduit);
         
-        this.fiber = fiber;
+        this.warning_e = new      IOWarning;
+        this.error_e   = this.new IOError;
         
-        this.exception = new IOException;
+        this.fiber = fiber;
     }
     
+    /**************************************************************************
+
+        Resumes the fiber coroutine and handles event. The fiber must be
+        suspended (HOLD state).
+        
+        Note that the fiber coroutine keeps going after this method has finished
+        if there is another instance of this class which shares the fiber with
+        this instance and is invoked in the coroutine after this instance has
+        done its job.
+        
+        Params:
+            event = I/O event
+            
+        Returns:
+            false if the fiber is finished or true if it keeps going
+    
+     **************************************************************************/
+
     final bool handle ( Event event )
+    in
     {
+        assert (this.fiber.state == this.fiber.State.HOLD);
+    }
+    body
+    {
+        this.event = event;
+        
         this.fiber.call();
+        
         return this.fiber.state != this.fiber.State.TERM;
     }
     
-    static class IOException : TangoException.IOException
+    /**************************************************************************
+
+        (Re)starts the fiber coroutine.
+            
+        Returns:
+            this instance
+    
+     **************************************************************************/
+
+    public typeof (this) start ( )
     {
-        int errnum = 0;
+        if (this.fiber.state == this.fiber.State.TERM)
+        {
+            this.fiber.reset();
+        }
         
-        this ( ) {super("");}
+        this.fiber.call();
+        
+        return this;
+    }
+    
+    /**************************************************************************
+
+        Suspends the fiber coroutine. The fiber must be running (EXEC state).
+            
+        Returns:
+            this instance
+    
+     **************************************************************************/
+
+    public typeof (this) suspend ( )
+    in
+    {
+        assert (this.fiber.state == this.fiber.State.EXEC);
+    }
+    body
+    {
+        this.fiber.cede();
+        
+        return this;
+    }
+    
+    /**************************************************************************
+
+        Returns:
+            current fiber state
+    
+     **************************************************************************/
+
+    public Fiber.State state ( )
+    {
+        return this.fiber.state;
+    }
+    
+    /**************************************************************************
+
+        Repeatedly invokes again while again returns true; suspends the
+        coroutine if again indicates continuation.
+        The fiber must be running (EXEC state).
+        
+        Params:
+            again = expression returning true to suspend and be invoked again
+                    or false to quit
+        
+     **************************************************************************/
+
+    protected void repeat ( lazy bool again )
+    in
+    {
+        assert (this.fiber.state == this.fiber.State.EXEC);
+    }
+    body
+    {
+        while (again())
+        {
+            this.suspend();
+        }
+    }
+    
+    /**************************************************************************
+
+        IOWarning class; to be thrown on end-of-flow conditions without an
+        error reported by errno or a socket error.
+        
+     **************************************************************************/
+
+    static class IOWarning : IOException
+    {
+        /**********************************************************************
+
+            Throws this instance if ok is false, 0 or null.
+            
+            Params:
+                ok   = condition that must not be false, 0 or null
+                msg  = message
+                file = source code file name
+                line = source code line
+            
+            Throws:
+                this instance if ok is false, 0 or null
+            
+         **********************************************************************/
         
         void assertEx ( T ) ( T ok, char[] msg, char[] file = "", long line = 0 )
         {
-            if (!ok) throw this.opCall(msg, file, line);
+            if (!ok)
+            {
+                super.set(msg, file, line);
+                throw this;
+            }
         }
         
-        typeof (this) opCall ( char[] msg, char[] file = "", long line = 0 )
+        /**********************************************************************
+
+            Queries and resets errno and sets the exception parameters.
+            
+            Params:
+                msg  = message
+                file = source code file name
+                line = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        public typeof (this) opCall ( char[] msg, char[] file = "", long line = 0 )
         {
-            this.errnum = errno;
+            super.set(msg, file, line);
+            return this;
+        }
+        
+        /**********************************************************************
+
+            Sets the exception parameters.
+            
+            Params:
+                errnum = error number
+                msg    = message
+                file   = source code file name
+                line   = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        public typeof (this) opCall  ( int errnum, char[] msg, char[] file = "", long line = 0 )
+        {
+            super.set(errnum, msg, file, line);
+            return this;
+        }
+    }
+    
+    /**************************************************************************
+
+        IOError class; to be thrown on end-of-flow conditions with an error
+        reported by errno or a socket error.
+        
+     **************************************************************************/
+
+    class IOError : IOException
+    {
+        /**********************************************************************
+
+            Queries and resets errno and sets the exception parameters.
+            
+            Params:
+                msg  = message
+                file = source code file name
+                line = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        public typeof (this) opCall ( char[] msg, char[] file = "", long line = 0 )
+        {
+            super.set(msg, file, line);
+            return this;
+        }
+        
+        /**********************************************************************
+    
+            Sets the exception parameters.
+            
+            Params:
+                errnum = error number
+                msg    = message
+                file   = source code file name
+                line   = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        public typeof (this) opCall  ( int errnum, char[] msg, char[] file = "", long line = 0 )
+        {
+            super.set(errnum, msg, file, line);
+            return this;
+        }
+        
+        /**********************************************************************
+        
+            Checks the socket error state of the conduit of the outer instance.
+            Does nothing if the conduit is not a socket. 
+             
+            Params:
+                msg    = message
+                file   = source code file name
+                line   = source code line
+            
+            Throws:
+                this instance if an error is reported for the conduit of the
+                outer instance
+            
+         **********************************************************************/
+        
+        void checkSocketError ( char[] msg, char[] file = "", long line = 0 )
+        {
+            with (Berkeley(cast (socket_t) this.outer.conduit.fileHandle))
+            {
+                int errnum = error;
+                
+                if (errnum)
+                {
+                    super.set(errnum, msg, file, line);
+                    throw this; 
+                }
+            }
+        }
+    }
+    
+    /**************************************************************************
+
+        IOException class; base class for IOWarning and IOError
+    
+     **************************************************************************/
+    
+    static class IOException : TangoException.IOException
+    {
+        /**********************************************************************
+
+            This alias
+        
+         **********************************************************************/
+        
+        alias typeof (this) This;
+        
+        /**********************************************************************
+
+            Error number
+        
+         **********************************************************************/
+        
+        int errnum = 0;
+        
+        /**********************************************************************
+
+            Constructor
+        
+         **********************************************************************/
+        
+        this ( ) {super("");}
+        
+        /**********************************************************************
+
+            Queries and resets errno and sets the exception parameters.
+            
+            Params:
+                msg  = message
+                file = source code file name
+                line = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        protected void set ( char[] msg, char[] file = "", long line = 0 )
+        {
+            scope (exit) .errno = 0;
+            
+            this.set(.errno, msg, file, line);
+        }
+        
+        /**********************************************************************
+        
+            Sets the exception parameters.
+            
+            Params:
+                errnum = error number
+                msg    = message
+                file   = source code file name
+                line   = source code line
+            
+            Returns:
+                this instance
+            
+         **********************************************************************/
+        
+        protected void set ( int errnum, char[] msg, char[] file = "", long line = 0 )
+        {
+            this.errnum = errnum;
             
             if (this.errnum)
             {
@@ -70,8 +482,6 @@ abstract class ISelectProtocol : IAdvancedSelectClient
             
             super.file.copy(file);
             super.line = line;
-            
-            return this;
         }
     }
 }
