060c15ab5609c64e09a183a2c7deea1db6937879
Mathias Baumann <mathias.baumann@sociomantic.com>
2013-01-07 16:52:43 +0000

    * RefuseRoot didn't actually work yet. Fixed & Added documentation

--- ocean/util/app/ext/RefuseRootExt.d
+++ ocean/util/app/ext/RefuseRootExt.d
@@ -1,9 +1,33 @@
+/*******************************************************************************
+
+    Arguments extension to refuse startup of the process if run as root.
+
+    copyright:      Copyright (c) 2013 sociomantic labs. All rights reserved
+
+    authors:        Mathias Baumann
+
+*******************************************************************************/
+
 module ocean.util.app.ext.RefuseRootExt;
 
 import ocean.util.app.ext.model.IArgumentsExtExtension;
+import tango.stdc.posix.unistd;
+
+/*******************************************************************************
+
+    Arguments extension that refuses to start if the program is run as root.
+    Behavior can be overridden by specifying --asroot
+
+*******************************************************************************/
 
 class RefuseRootExt : IArgumentsExtExtension
-{    
+{  
+    /***************************************************************************
+
+        Order doesn't matter, so return default -> 0
+
+    ***************************************************************************/
+      
     int order ()
     {
         return 0;
@@ -43,9 +67,14 @@ class RefuseRootExt : IArgumentsExtExtension
 
     char[] validateArgs ( IApplication app, Arguments args )
     {
-        if ( args.exists("asroot") ) return null;
-        
-        return "Won't run as root!";
+        if ( getuid() == 0 && !args.exists("asroot"))
+        {        
+            return "Won't run as root! (use --asroot if you really need to do this)";
+        }
+        else
+        {
+            return null;
+        }
     }
 
 
@@ -53,6 +82,8 @@ class RefuseRootExt : IArgumentsExtExtension
 
         Function executed after (successfully) validating the command line
         arguments.
+    
+        Exists to satisfy the interface.
 
         Params:
             app = application instance
