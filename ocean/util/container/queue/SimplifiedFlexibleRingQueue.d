7f7c2a9cbd8f20ebe437ae297555d0d673bdcc3a
Mathias Baumann <mathias.baumann@sociomantic.com>
2013-01-30 19:16:11 +0000

    Simpl.Flex.RingQue: Remove trailing whitespaces

--- ocean/util/container/queue/SimplifiedFlexibleRingQueue.d
+++ ocean/util/container/queue/SimplifiedFlexibleRingQueue.d
@@ -49,7 +49,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
         Location of the gap at the rear end of the data array where the unused
         space starts.
 
-    ***************************************************************************/        
+    ***************************************************************************/
 
     private size_t gap;
 
@@ -58,7 +58,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
 
         Header for queue items
 
-    ***************************************************************************/    
+    ***************************************************************************/
 
     private struct Header
     {
@@ -86,7 +86,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
     /***************************************************************************
 
         Constructor. The queue's memory buffer is allocated by the GC.
-        
+
         Params:
             dimension = size of queue in bytes
 
@@ -121,32 +121,32 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
 
 
     /***************************************************************************
-    
+
         Pushes an item into the queue.
-    
+
         Params:
             item = data item to push
-    
+
         Returns:
             true if the item was pushed successfully, false if it didn't fit
 
     ***************************************************************************/
-    
+
     public bool push ( ubyte[] item )
     {
         auto data = this.push(item.length);
-        
+
         if ( data is null )
         {
             return false;
         }
-        
+
         data[] = item[];
-        
-        return true;        
+
+        return true;
     }
-    
-    
+
+
     /***************************************************************************
 
         Acquires space in the queue into which a new item can be written.
@@ -166,54 +166,54 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
         {
             Header h; h.length = size;
             ubyte[] header = (cast(ubyte*)&h)[0 .. Header.sizeof];
-    
+
             if (this.needsWrapping(size))
             {
                 this.gap = super.write_to;
-                super.write_to = 0;            
+                super.write_to = 0;
             }
-            
+
             super.data[super.write_to .. super.write_to + header.length] = header[];
-            
+
             auto pos = super.write_to + header.length;
-          
+
             super.write_to += this.pushSize(size);
             super.items++;
-                    
+
             return super.data[pos .. pos + size];
         }
-        
+
         return null;
     }
-    
-    
+
+
     /***************************************************************************
 
         Peeks at the item that would be popped next.
-    
+
         Returns:
-            item that would be popped from queue, 
+            item that would be popped from queue,
             may be null if queue is empty
 
     ***************************************************************************/
 
     public ubyte[] peek ( )
-    {     
+    {
         auto read_pos = super.read_from;
-        
+
         if (read_pos >= this.gap)                                  // check whether there is an item at this offset
         {
             read_pos = 0;                                          // if no, set it to the beginning (wrapping around)
-        }        
-        
+        }
+
         Header* header = cast(Header*) (this.data.ptr + read_pos);
-          
+
         auto pos = read_pos + header.sizeof;
 
         return this.data[pos .. pos + header.length];
     }
-    
-    
+
+
     /***************************************************************************
 
         Pops an item from the queue.
@@ -226,15 +226,15 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
     public ubyte[] pop ( )
     {
         if ( this.items == 0 ) return null;
-        
+
         if ( super.read_from >= this.gap )                                  // check whether there is an item at this offset
         {
             super.read_from = 0;                                          // if no, set it to the beginning (wrapping around)
             this.gap = super.data.length;
         }
-        
+
         Header* header = cast(Header*) (this.data.ptr + this.read_from);
-                
+
         auto pos = super.read_from + header.sizeof;
 
         super.items--;
@@ -247,7 +247,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
         else
         {
             super.read_from += this.pushSize(header.length);
-            
+
             if ( super.read_from >= super.data.length )
             {
                 super.read_from = 0;
@@ -270,12 +270,12 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
         {
             return 0;
         }
-        
+
         if (super.write_to > super.read_from)
         {
             return super.write_to - super.read_from;
         }
-        
+
         return this.gap - super.read_from + super.write_to;
     }
 
@@ -320,10 +320,10 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
 
         Params:
             bytes = number of bytes to calculate push size of
-    
+
         Returns:
             number of bytes the item would take up if pushed to the queue
-    
+
     ***************************************************************************/
 
     static public size_t pushSize ( size_t bytes )
@@ -335,7 +335,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
     /***************************************************************************
 
         Finds out whether the provided item will fit in the queue. Also
-        considers the need of wrapping. 
+        considers the need of wrapping.
 
         Params:
             item = item to check
@@ -361,7 +361,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
         calculate the item's push size.
 
         Params:
-            bytes = size of item to check 
+            bytes = size of item to check
 
         Returns:
             true if the bytes fits, else false
@@ -382,7 +382,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
             {
                 long d = super.read_from - super.write_to;
 
-                return push_size <= d || d < 0; 
+                return push_size <= d || d < 0;
             }
         }
         else
@@ -396,51 +396,51 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
     /***************************************************************************
 
         Writes the queue's state and contents to the given output stream.
-    
+
         Params:
             stream = output to write to
-    
+
         Returns:
             number of bytes written
-    
+
     ***************************************************************************/
-    
+
     public size_t serialize ( OutputStream stream )
     {
         size_t bytes;
-    
+
         bytes += SimpleSerializer.write(stream, this.gap);
         bytes += SimpleSerializer.write(stream, super.write_to);
         bytes += SimpleSerializer.write(stream, super.read_from);
         bytes += SimpleSerializer.write(stream, super.items);
         bytes += SimpleSerializer.write(stream, super.data);
-    
+
         return bytes;
     }
-    
-    
+
+
     /***************************************************************************
-    
+
         Reads the queue's state and contents from the given input stream.
-    
+
         Params:
             stream = input to read from
-    
+
         Returns:
             number of bytes read
-    
+
     ***************************************************************************/
-    
+
     public size_t deserialize ( InputStream stream )
     {
         size_t bytes;
-    
+
         bytes += SimpleSerializer.read(stream, this.gap);
         bytes += SimpleSerializer.read(stream, super.write_to);
         bytes += SimpleSerializer.read(stream, super.read_from);
         bytes += SimpleSerializer.read(stream, super.items);
         bytes += SimpleSerializer.read(stream, super.data);
-    
+
         return bytes;
     }
 
@@ -460,7 +460,7 @@ class FlexibleByteRingQueue : IRingQueue!(IByteQueue)
 
     private bool needsWrapping ( size_t bytes )
     {
-        return pushSize(bytes) + super.write_to > super.data.length;           
+        return pushSize(bytes) + super.write_to > super.data.length;
     }
 }
 
@@ -475,15 +475,15 @@ unittest
     scope queue = new FlexibleByteRingQueue((9+FlexibleByteRingQueue.Header.sizeof)*10);
     assert(!queue.free_space == 0);
     assert(queue.is_empty);
-    
+
     assert(queue.push(cast(ubyte[])"Element 1"));
     assert(queue.pop() == cast(ubyte[])"Element 1");
     assert(queue.items == 0);
     assert(!queue.free_space == 0);
     assert(queue.is_empty);
     assert(queue.used_space() == 0);
-    
-    assert(queue.push(cast(ubyte[])"Element 1"));        
+
+    assert(queue.push(cast(ubyte[])"Element 1"));
     assert(queue.push(cast(ubyte[])"Element 2"));
     assert(queue.push(cast(ubyte[])"Element 3"));
     assert(queue.push(cast(ubyte[])"Element 4"));
@@ -493,20 +493,20 @@ unittest
     assert(queue.push(cast(ubyte[])"Element 8"));
     assert(queue.push(cast(ubyte[])"Element 9"));
     assert(queue.push(cast(ubyte[])"Element10"));
-    
+
     assert(queue.length == 10);
     assert(queue.free_space == 0);
     assert(!queue.is_empty);
-    
+
     assert(!queue.push(cast(ubyte[])"more"));
     assert(queue.length == 10);
-    
-    scope middle = new FlexibleByteRingQueue((1+FlexibleByteRingQueue.Header.sizeof)*5);        
-    middle.push(cast(ubyte[])"1");        
+
+    scope middle = new FlexibleByteRingQueue((1+FlexibleByteRingQueue.Header.sizeof)*5);
+    middle.push(cast(ubyte[])"1");
     middle.push(cast(ubyte[])"2");
     middle.push(cast(ubyte[])"3");
     middle.push(cast(ubyte[])"4");
-    assert(middle.pop == cast(ubyte[])"1");        
+    assert(middle.pop == cast(ubyte[])"1");
     assert(middle.read_from == 1 + FlexibleByteRingQueue.Header.sizeof);
     assert(middle.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*4);
     assert(middle.free_space() == (1+FlexibleByteRingQueue.Header.sizeof)*2);
@@ -525,12 +525,12 @@ debug ( OceanUnitTest )
 {
     // Uncomment the next line to see UnitTest output
     // debug = Verbose;
-    
+
     import tango.math.random.Random;
     import tango.time.StopWatch;
     import tango.core.Memory;
-    import tango.io.FilePath; 
-    import ocean.util.log.Trace; 
+    import tango.io.FilePath;
+    import ocean.util.log.Trace;
 
     unittest
     {
@@ -541,119 +541,119 @@ debug ( OceanUnitTest )
             Test for empty queue
 
         ***********************************************************************/
-        
+
         {
            auto caught = false;
-           try scope queue = new FlexibleByteRingQueue(0); 
+           try scope queue = new FlexibleByteRingQueue(0);
            catch ( Exception e ) caught = true;
            assert (caught == true);
         }
 
         /***********************************************************************
-            
+
             Test wrapping
-        
-        ***********************************************************************/        
+
+        ***********************************************************************/
 
         debug (Verbose)  Trace.formatln("\nRunning ocean.io.device.queue.FlexibleByteRingQueue wrapping stability test");
         {
             scope queue = new FlexibleByteRingQueue((1+FlexibleByteRingQueue.Header.sizeof)*3);
-    
+
             assert(queue.read_from == 0);
             assert(queue.write_to == 0);
             // [___] r=0 w=0
             assert(queue.push(cast(ubyte[])"1"));
-                 
+
             assert(queue.read_from == 0);
             assert(queue.write_to == 1+FlexibleByteRingQueue.Header.sizeof);
             assert(queue.items == 1);
             assert((cast(FlexibleByteRingQueue.Header*) queue.data.ptr).length == 1);
-            
-            assert(queue.data[FlexibleByteRingQueue.Header.sizeof .. 
-                              1+FlexibleByteRingQueue.Header.sizeof] == 
+
+            assert(queue.data[FlexibleByteRingQueue.Header.sizeof ..
+                              1+FlexibleByteRingQueue.Header.sizeof] ==
                                   cast(ubyte[]) "1");
-                        
+
             // [#__] r=0 w=5
             assert(queue.push(cast(ubyte[])"2"));
             assert(queue.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*2);
 
-            assert((cast(FlexibleByteRingQueue.Header*) 
+            assert((cast(FlexibleByteRingQueue.Header*)
                 (queue.data.ptr + 1 + FlexibleByteRingQueue.Header.sizeof)).length == 1);
-            
-            assert(queue.data[1+FlexibleByteRingQueue.Header.sizeof*2 .. 
-                              1+FlexibleByteRingQueue.Header.sizeof*2+1] == 
+
+            assert(queue.data[1+FlexibleByteRingQueue.Header.sizeof*2 ..
+                              1+FlexibleByteRingQueue.Header.sizeof*2+1] ==
                                   cast(ubyte[]) "2");
             //assert(queue.data[queue.write_to-1-FlexibleByteRingQueue.h .. ])
-            
-            
-            
+
+
+
             // [##_] r=0 w=10
             assert(queue.push(cast(ubyte[])"3"));
             assert(queue.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*3);
-            
+
             // [###] r=0 w=15
             assert(!queue.push(cast(ubyte[])"4"));
             assert(queue.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*3);
             assert(queue.read_from == 0);
-            
+
             assert(queue.free_space == 0);
             assert(queue.pop() == cast(ubyte[])"1");
-            
+
             assert(queue.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*3);
             assert(queue.read_from == 1+FlexibleByteRingQueue.Header.sizeof);
-            
+
             // [_##] r=5 w=15
             assert(queue.free_space() == 1+FlexibleByteRingQueue.Header.sizeof);
-            
+
             assert(queue.write_to == (1+FlexibleByteRingQueue.Header.sizeof)*3);
             assert(queue.read_from == 1+FlexibleByteRingQueue.Header.sizeof);
 
             assert(queue.pop() == cast(ubyte[])"2");
-            
+
             // [__#] r=10 w=15
             assert(queue.free_space() == (1+FlexibleByteRingQueue.Header.sizeof)*2);
             assert(queue.write_to == queue.data.length);
             assert(queue.push(cast(ubyte[])"1"));
-            
+
             // [#_#] r=10 w=5
             assert(queue.free_space() == 1+FlexibleByteRingQueue.Header.sizeof);
             assert(queue.write_to == queue.pushSize("2".length));
             assert(queue.push(cast(ubyte[])"2"));
            // Trace.formatln("gap is {}, free is {}, write is {}", queue.gap, queue.free_space(),queue.write_to);
-           
-            
+
+
             // [###] r=10 w=10
             assert(queue.free_space == 0);
             assert(queue.pop() == cast(ubyte[])"3");
-      
+
             // [##_] r=15/0 w=10
             assert(queue.free_space() == (1+FlexibleByteRingQueue.Header.sizeof)*1);
-            assert(queue.pop() == cast(ubyte[])"1");         
-    
+            assert(queue.pop() == cast(ubyte[])"1");
+
             // [_#_] r=5 w=10
             assert(queue.pop() == cast(ubyte[])"2");
-    
+
             // [__] r=0 w=0
             assert(queue.is_empty);
             assert(queue.push(cast(ubyte[])"1"));
-    
+
             // [#__] r=0 w=5
-            assert(queue.push(cast(ubyte[])"2#"));            
-            
+            assert(queue.push(cast(ubyte[])"2#"));
+
             // [#$_] r=0 w=11 ($ = 2 bytes)
-            assert(queue.pop() == cast(ubyte[])"1");           
-            
+            assert(queue.pop() == cast(ubyte[])"1");
+
             // [_$_] r=5 w=11
-            assert(queue.push(cast(ubyte[])"1"));             
-            
+            assert(queue.push(cast(ubyte[])"1"));
+
             // [#$_] r=5 w=5
             assert(!queue.push(cast(ubyte[])"2"));
             assert(queue.pop() == cast(ubyte[])"2#");
-    
+
             // [#__] r=11 w=5
             assert(queue.push(cast(ubyte[])"2")); // this needs to be wrapped now
-    
-            // [##_] r=11 w=10            
+
+            // [##_] r=11 w=10
             assert(queue.gap == queue.read_from);
         }
     }
