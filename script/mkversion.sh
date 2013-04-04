89ac04ef10fffac657ef56c1f235d34f2fc6368c
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2013-04-04 21:15:40 +0200

    mkversion: Change indentation to use 4 spaces

--- script/mkversion.sh
+++ script/mkversion.sh
@@ -12,19 +12,19 @@ date_cmd="date +%Y-%m-%d"
 
 print_usage()
 {
-	echo "\
+    echo "\
 Generates a Version.d file.
 
 Usage: $0 [OPTIONS] [GC] [LIB1] [LIB2] ...
 
 Options:
 
--o FILE		Where to write the output (Version.d) file (default: $rev_file)
--L DIR		Directory where to find the libraries (default: $lib_dir)
--a AUTHOR	Author of the build (default: detected, currently $author)
--t TEMPLATE	Template file to use (default: [DIR]/ocean/script/Version.d.tpl)
--d DATE		Build date string (default: output of '$date_cmd')
--h		Shows this help and exit
+-o FILE      Where to write the output (Version.d) file (default: $rev_file)
+-L DIR       Directory where to find the libraries (default: $lib_dir)
+-a AUTHOR    Author of the build (default: detected, currently $author)
+-t TEMPLATE  Template file to use (default: [DIR]/ocean/script/Version.d.tpl)
+-d DATE      Build date string (default: output of '$date_cmd')
+-h           Shows this help and exit
 
 GC is the garbage collector used to compile the program (should be either 'cdgc'
 or 'basic').
@@ -45,8 +45,8 @@ do
         o)  rev_file="$OPTARG";;
         L)  lib_dir="$OPTARG";;
         a)  author="$OPTARG";;
-	t)  template="$OPTARG";;
-	d)  date="$OPTARG";;
+        t)  template="$OPTARG";;
+        d)  date="$OPTARG";;
         h)  print_usage ; exit 0;;
         \?) echo >&2; print_usage >&2; exit 2;;
     esac
@@ -60,27 +60,27 @@ test -z "$date" && date="`$date_cmd`"
 # Check if git is installed
 git=`which git`
 test -z "$git" && {
-	echo "$0: Can't find git command! Aborting..." >&2
-	exit 1
+    echo "$0: Can't find git command! Aborting..." >&2
+    exit 1
 }
 
 get_rev()
 {
-	# Check if we are pointing to the right directory
-	cd "$1"
-	$git rev-parse --git-dir > /dev/null || {
-		echo "$0: $1 is not a git repository! Aborting..." >&2
-		exit 1
-	}
-	branch=`$git describe --exact-match 2> /dev/null`
-	test -z "$branch" &&
-		branch=`$git rev-parse --abbrev-ref HEAD`
-	test "$branch" = HEAD &&
-		branch=DETACHED
-	printf $branch-`$git rev-parse --short HEAD`
-	test -n "`$git status --porcelain -uno`" &&
-		printf '*'
-	cd - > /dev/null
+    # Check if we are pointing to the right directory
+    cd "$1"
+    $git rev-parse --git-dir > /dev/null || {
+        echo "$0: $1 is not a git repository! Aborting..." >&2
+        exit 1
+    }
+    branch=`$git describe --exact-match 2> /dev/null`
+    test -z "$branch" &&
+        branch=`$git rev-parse --abbrev-ref HEAD`
+    test "$branch" = HEAD &&
+        branch=DETACHED
+    printf $branch-`$git rev-parse --short HEAD`
+    test -n "`$git status --porcelain -uno`" &&
+        printf '*'
+    cd - > /dev/null
 }
 
 tmp=`mktemp mkversion.XXXXXXXXXX`
@@ -92,11 +92,11 @@ cp "$template" "$tmp"
 module=`echo "$rev_file" | sed -e 's|/|.|g' -e 's|.d||g'`
 gc="$1"; shift
 sed -i "$tmp" \
-	-e "s/@MODULE@/$module/" \
-	-e "s/@GC@/$gc/" \
-	-e "s/@REVISION@/`get_rev .`/" \
-	-e "s/@DATE@/$date/" \
-	-e "s/@AUTHOR@/$author/" \
+    -e "s/@MODULE@/$module/" \
+    -e "s/@GC@/$gc/" \
+    -e "s/@REVISION@/`get_rev .`/" \
+    -e "s/@DATE@/$date/" \
+    -e "s/@AUTHOR@/$author/" \
     -e "s/@DMD@/$dmd/"
 
 # Generate the libraries info
@@ -110,12 +110,14 @@ sed -i "s/@LIBRARIES@/$libs/" "$tmp"
 # Check if anything has changed
 if [ -e "$rev_file" ]
 then
-	sum1=`md5sum "$tmp" | cut -d' ' -f1`
-	sum2=`md5sum "$rev_file" | cut -d' ' -f1`
-	if [ $sum1 = $sum2 ]
-	then
-		rm "$tmp"
-		exit 0
-	fi
+    sum1=`md5sum "$tmp" | cut -d' ' -f1`
+    sum2=`md5sum "$rev_file" | cut -d' ' -f1`
+    if [ $sum1 = $sum2 ]
+    then
+        rm "$tmp"
+        exit 0
+    fi
 fi
 mv "$tmp" "$rev_file"
+
+# vim: set et sw=4 sts=4 :
