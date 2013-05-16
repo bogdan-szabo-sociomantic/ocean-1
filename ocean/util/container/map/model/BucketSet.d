144d7f3f46f47cb8b8b14400f9958ee818527b28
Gavin Norman <gavin.norman@sociomantic.com>
2013-05-16 15:58:32 +0200

    Comment correction
    ctor parameters were not described correctly.

--- ocean/util/container/map/model/BucketSet.d
+++ ocean/util/container/map/model/BucketSet.d
@@ -89,7 +89,14 @@ public abstract class IBucketSet
         Constructor.
 
         Params:
-            bucket_elements_info = Pool of bucket elements
+            n = expected number of elements in mapping
+            load_factor = ratio of n to the number of internal buckets. The
+                desired (approximate) number of elements per bucket. For
+                example, 0.5 sets the number of buckets to double n; for 2 the
+                number of buckets is the half of n. load_factor must be greater
+                than 0. The load factor is basically a trade-off between memory
+                usage (number of buckets) and search time (number of elements
+                per bucket).
 
      **************************************************************************/
 
