fcc2a9595dee9f4fde5fe9083a0247cdf2be06a7
Gavin Norman <gavin.norman@sociomantic.com>
2013-09-20 12:19:58 +0200

    Remove unused and untested method
    Something like this can be added back into the class at some point, if
    needed.

--- ocean/text/regex/PCRE.d
+++ ocean/text/regex/PCRE.d
@@ -104,14 +104,6 @@ class PCRE
 
     /***************************************************************************
 
-        A reusable int buffer
-
-    ***************************************************************************/
-
-    protected int[] buffer_int;
-
-    /***************************************************************************
-
         A re-usable exception instance
 
     ***************************************************************************/
@@ -180,113 +172,5 @@ class PCRE
         }
 
         return false;
-    }
-
-
-
-    /***************************************************************************
-
-        Perform a global regular expression match
-
-        FIXME:
-            THe method wasn't recently tested for functionality correctness or
-            for effecient memory usage (due to absence of a test example).
-            The user should test both the functionality and memory usage of the
-            method before using it.
-
-        Usage:
-
-            char[][][] matches;
-
-            auto regex = new PCRE;
-
-            char[] preg = "Hello";
-            char[] subj = "Hello World Hello Word";
-
-            int i = regex.preg_match_all(subj, preg, matches);
-
-            foreach ( match; matches )
-            {
-                foreach ( element; match )
-                    Stdout.format("{}", element);
-
-                Stdout.newline();
-            }
-
-        Params:
-            string  = input string (subject)
-            pattern = pattern to search for, as a string
-            matches = array to store matches into
-            icase   = case sensitive matching
-
-        Returns:
-            zero, or number of matches
-
-    ***************************************************************************/
-
-    public int preg_match_all ( char[] string, char[] pattern, inout char[][][] matches, bool icase = false )
-    {
-        int   error, count, num_matches, start_offset, ovector_length;
-        char* errmsg, stringptr;
-        pcre* re;
-        scope (exit) free(re);
-
-        int*  ovector;
-
-        this.buffer_char.copy(pattern);
-        if ((re = pcre_compile(StringC.toCstring(this.buffer_char),
-                (icase ? PCRE_CASELESS : 0), &errmsg, &error, null)) == null)
-        {
-            this.buffer_char.length = 0;
-            Layout!(char).print(this.buffer_char, "Couldn't compile regular "
-                "expression: {} - on pattern: {}", StringC.toDString(errmsg),
-                pattern);
-            this.exception.setMsg(this.buffer_char);
-            throw this.exception;
-        }
-
-        if ( pcre_fullinfo(re, null, PCRE_INFO_CAPTURECOUNT, &ovector_length) < 0 )
-        {
-            this.exception.setMsg("Internal pcre_fullinfo() error");
-            throw this.exception;
-        }
-
-        ovector_length = (ovector_length + 1) * 3;
-        this.buffer_int.length = ovector_length;
-        ovector = this.buffer_int.ptr;
-
-        do
-        {
-            this.buffer_char.copy(string);
-            count = pcre_exec(re, null, StringC.toCstring(this.buffer_char),
-                string.length, start_offset, 0, ovector, ovector_length);
-
-            if ( count > 0 )
-            {
-                char[][] match_item;
-
-                ++num_matches;
-
-                for ( int i = 0; i < count; i++ )
-                {
-                    pcre_get_substring(StringC.toCstring(this.buffer_char),
-                        ovector, count, i, &stringptr);
-                    match_item ~= StringC.toDString(stringptr).dup;
-                    pcre_free_substring(stringptr);
-                }
-
-                matches ~= match_item;
-            }
-            else if (count != PCRE_ERROR_NOMATCH)
-            {
-                this.exception.setMsg("Error on executing regular expression!");
-                throw this.exception;
-            }
-
-            start_offset = cast(int) ovector[1];
-        }
-        while ( count > 0 );
-
-        return num_matches;
     }
 }
