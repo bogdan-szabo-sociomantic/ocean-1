fad9b2aa58da09f488620c691e11e780ff5c688e
Stefan Brus <stefan.brus@sociomantic.com>
2013-10-16 16:09:10 +0200

    Add unittests to SlidingAverage
    Adds unittests to the SlidingAverage class

--- ocean/math/SlidingAverage.d
+++ ocean/math/SlidingAverage.d
@@ -190,6 +190,75 @@ public class SlidingAverage ( T )
 
 /*******************************************************************************
 
+    SlidingAverage unittests
+
+*******************************************************************************/
+
+debug private import tango.util.Convert: to;
+
+
+/*******************************************************************************
+
+    Runs a series of tests a SlidingAverage of the given type and size
+    Template function so no code will be generated unless in debug mode.
+
+    Params:
+        size = The number of values to fill the SlidingAverage with.
+        test_iteration = The current test iteration, for error messages.
+
+*******************************************************************************/
+
+debug void runTests ( T ) ( uint size, int test_iteration )
+{
+    assert(size > 2, "can only test SlidingAverages with at least 2 values");
+
+    auto avg = new SlidingAverage!(T)(size);
+
+    char[] err_prefix = "SlidingAverage assertion failed for iteration " ~
+        to!(char[])(test_iteration) ~ ": ";
+
+    ulong sum;
+
+    for ( int i = 1; i <= size; i++ )
+    {
+        avg.push(i);
+        sum += i;
+    }
+
+    // Test a full SlidingAverage
+    assert(avg.average == cast(double)sum / size, err_prefix ~ "test 1");
+
+    // Add size + 1 to the average, but only size to the sum
+    // Because at this point the first value of the average will be pushed out
+    avg.push(size + 1);
+    sum += size;
+
+    // Test a SlidingAverage where the oldest value has been replaced
+    assert(avg.average == cast(double)sum / size, err_prefix ~ "test 2");
+
+    avg.clear;
+
+    // Test if average is 0 upon clearing after filling it with values
+    assert(avg.average == 0, err_prefix ~ "test 3");
+
+    avg.push(2);
+    avg.push(4);
+
+    // Test a partially filled SlidingAverage
+    assert(avg.average == 3, err_prefix ~ "test 4");
+}
+
+
+unittest
+{
+    runTests!(ulong)(100, 1);
+    runTests!(int)(50, 2);
+    runTests!(double)(1000, 3);
+}
+
+
+/*******************************************************************************
+
     Sliding Average Time Class
 
     SlidingAverageTime offers a few more functions. It is for the use case when
