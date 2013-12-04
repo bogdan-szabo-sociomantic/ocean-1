bogdan-szabo-sociomantic/ocean-1

commit f1cea49cf2b4051843067d687c91ada7fe964ac3

Fix SlidingAverage logic
The average is now calculated using only the indexes that have actual
values in the sliding window.

ocean/math/SlidingAverage.d
@@ -70,6 +70,15 @@ public class SlidingAverage ( T )
 
     /***************************************************************************
 
+        The number of values the sliding window currently contains
+
+    ***************************************************************************/
+
+    protected size_t current_size;
+
+
+    /***************************************************************************
+
         Constructor
 
         Params:
@@ -78,6 +87,11 @@ public class SlidingAverage ( T )
     ***************************************************************************/
 
     public this ( size_t window_size )
+    in
+    {
+        assert(window_size > 1, "SlidingAverage, window_size parameter must be > 1");
+    }
+    body
     {
         this.window = new T[window_size];
     }
@@ -99,21 +113,32 @@ public class SlidingAverage ( T )
 
     public real push ( T value )
     {
-        if ( ++this.index >= this.window.length )
+        // overwrite oldest value if max slider size has been reached
+        if ( this.index >= this.window.length )
         {
             this.index = 0;
         }
 
         this.window[this.index] = value;
 
+        this.index++;
+
+        // only the filled indexes in the slider should be calculated
+        if ( this.current_size < this.window.length )
+        {
+            this.current_size++;
+        }
+
         this._average = 0.0;
 
-        foreach ( val; this.window )
+        foreach ( val; this.window[0 .. this.current_size] )
         {
             this._average += val;
         }
 
-        return this._average /= this.window.length;
+        this._average /= this.current_size;
+
+        return this._average;
     }
 
 
@@ -157,11 +182,8 @@ public class SlidingAverage ( T )
     {
         this.index = 0;
         this._average = 0;
-
-        foreach ( ref val; this.window )
-        {
-            val = 0;
-        }
+        this.current_size = 0;
+        this.window[] = 0;
     }
 }
 

commit f5fdbd4a6c5f787cf2840b19f11d0c30e3d7188f

Fix SlidingAverage bug introduced by f1cea49c
this.index must always be a valid value so it can be used with this.last()

ocean/math/SlidingAverage.d
@@ -94,6 +94,7 @@ public class SlidingAverage ( T )
     body
     {
         this.window = new T[window_size];
+        this.index = this.index.max;
     }
 
 
@@ -113,6 +114,7 @@ public class SlidingAverage ( T )
 
     public real push ( T value )
     {
+        this.index++;
         // overwrite oldest value if max slider size has been reached
         if ( this.index >= this.window.length )
         {
@@ -120,8 +122,6 @@ public class SlidingAverage ( T )
         }
 
         this.window[this.index] = value;
-
-        this.index++;
 
         // only the filled indexes in the slider should be calculated
         if ( this.current_size < this.window.length )
@@ -153,6 +153,11 @@ public class SlidingAverage ( T )
 
     public T last ( )
     {
+        if ( this.index > this.window.length )
+        {
+            return T.init;
+        }
+
         return this.window[this.index];
     }
 
@@ -176,11 +181,13 @@ public class SlidingAverage ( T )
 
         Resets the average counter to its initial state.
 
+        Index is set to max so that it will be set to 0 once .push() is called
+
     ***************************************************************************/
 
     public void clear ( )
     {
-        this.index = 0;
+        this.index = this.index.max;
         this._average = 0;
         this.current_size = 0;
         this.window[] = 0;
@@ -222,15 +229,18 @@ debug void runTests ( T ) ( uint size, int test_iteration )
     for ( int i = 1; i <= size; i++ )
     {
         avg.push(i);
+        assert ( avg.last == i, "last() didn't return last added value" ); // #220
         sum += i;
     }
 
     // Test a full SlidingAverage
     assert(avg.average == cast(double)sum / size, err_prefix ~ "test 1");
+    assert ( avg.last == size, "last() didn't return last added value" ); // #220
 
     // Add size + 1 to the average, but only size to the sum
     // Because at this point the first value of the average will be pushed out
     avg.push(size + 1);
+    assert ( avg.last == size+1, "last() didn't return last added value" ); // #220
     sum += size;
 
     // Test a SlidingAverage where the oldest value has been replaced
@@ -242,7 +252,9 @@ debug void runTests ( T ) ( uint size, int test_iteration )
     assert(avg.average == 0, err_prefix ~ "test 3");
 
     avg.push(2);
+    assert ( avg.last == 2 , "last() didn't return last added value" ); // #220
     avg.push(4);
+    assert ( avg.last == 4 , "last() didn't return last added value" ); // #220
 
     // Test a partially filled SlidingAverage
     assert(avg.average == 3, err_prefix ~ "test 4");