5c53876e142793f07fb4810bb019662826214ffd
Mathias L. Baumann <mathias.baumann@sociomantic.com>
2014-05-28 18:46:49 +0200

    Add documentation to FirstName module

--- src/ocean/io/digest/FirstName.d
+++ src/ocean/io/digest/FirstName.d
@@ -1,10 +1,57 @@
+/******************************************************************************
+
+    Map numbers to easier distinguishable names
+
+    copyright: Copyright (c) 2014 sociomantic labs. All rights reserved
+
+    author:    Mathias L. Baumann
+
+*******************************************************************************/
+
 module ocean.io.digest.FirstName;
+
+/*******************************************************************************
+
+    Imports
+
+*******************************************************************************/
 
 import ocean.io.digest.Fnv1;
 
+/*******************************************************************************
+
+    Class to provide number -> name translation
+
+    This class is useful for when you want to display a lot of numbers that
+    don't have an immediate meaning for a person (pointers, hashes, etc) but
+    where it still is helpful or important to easily see whether a number is
+    different from the other.
+
+    Usage Example:
+    ---
+
+    import ocean.io.digest.FirstName;
+    import ocean.util.log.Trace;
+
+    MyClass myArray[] = ... ;
+
+
+    foreach ( c; myArray )
+    {
+        Trace.formatln("{}.property = {}", FirstName(cast(void*) c, c.property);
+    }
+    ---
+
+*******************************************************************************/
 
 static class FirstName
 {
+    /***************************************************************************
+
+        Static list of strings (preferably names) that a number can be mapped to
+
+    ***************************************************************************/
+
     static private char[][] names =
             ["Sarah",
             "David",
@@ -46,6 +93,21 @@ static class FirstName
             "Storm",
             "Walter",
             "Fawfzi"];
+
+    /***************************************************************************
+
+        Function to map an abitrary integer to a string for easier distinction
+
+        Template Params:
+            T = integer type, should be Fnv1a compatible
+
+        Params:
+            value = an integer value to map
+
+        Returns:
+            a string matching the hash of the given number
+
+    ***************************************************************************/
 
     static char[] opCall ( T ) ( T value )
     {
