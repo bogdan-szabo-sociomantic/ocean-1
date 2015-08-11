bogdan-szabo-sociomantic/ocean-1

commit 3e74e183afbdeba1db3432bfb268461d0751f6ec

Make TimerEvent a TimerFD wrapper
Fix #639: TimerEvent and TimerFD contain mostly the same code. Remove the
duplication.

src/ocean/io/select/client/TimerEvent.d
@@ -20,172 +20,14 @@ module ocean.io.select.client.TimerEvent;
 
 import ocean.io.select.client.model.ISelectClient: ISelectClient;
 
-import ocean.sys.ErrnoException;
 import ocean.core.Array : copy;
+import ocean.sys.TimerFD;
 import ocean.core.Traits;
 
 import tango.io.model.IConduit: ISelectable;
 
-import tango.stdc.posix.time: time_t, timespec, itimerspec, CLOCK_REALTIME;
-
-import tango.stdc.posix.sys.types: ssize_t;
-
-import tango.stdc.posix.unistd: read, write, close;
-
-import tango.stdc.errno: EAGAIN, EWOULDBLOCK, errno;
-
 import tango.text.convert.Format;
-
-/// <sys/timerfd.h>
-
-const TFD_TIMER_ABSTIME = 1,
-      TFD_CLOEXEC       = 0x80000, // octal 02000000
-      TFD_NONBLOCK      = 0x800;   // octal 04000
-
-/// <linux/time.h>
-
-const CLOCK_MONOTONIC = 1;
-
-// TODO: move into C bindings after tango/ocean merge
-
-extern (C)
-{
-    /**************************************************************************
-
-        Creates a new timer object.
-
-        The file descriptor supports the following operations:
-
-        read(2)
-               If  the timer has already expired one or more times since its
-               settings were last modified using timerfd_settime(), or since the
-               last successful read(2), then the buffer given to read(2) returns
-               an unsigned 8-byte integer (uint64_t) containing  the  number of
-               expirations that have occurred.  (The returned value is in host
-               byte order, i.e., the native byte order for integers on the host
-               machine.)
-
-               If no timer expirations have occurred at the time of the read(2),
-               then the call either blocks until the next timer  expiration, or
-               fails with the error EAGAIN if the file descriptor has been made
-               non-blocking (via the use of the fcntl(2) F_SETFL operation to
-               set the O_NONBLOCK flag).
-
-               A read(2) will fail with the error EINVAL if the size of the
-               supplied buffer is less than 8 bytes.
-
-        poll(2), select(2) (and similar)
-               The file descriptor is readable (the select(2) readfds argument;
-               the poll(2) POLLIN flag) if one or more timer expirations have
-               occurred.
-
-               The file descriptor also supports the other file-descriptor
-               multiplexing APIs: pselect(2), ppoll(2), and epoll(7).
-
-        close(2)
-               When  the  file descriptor is no longer required it should be
-               closed.  When all file descriptors associated with the same timer
-               object have been closed, the timer is disarmed and its resources
-               are freed by the kernel.
-
-        fork(2) semantics
-            After a fork(2), the child inherits a copy of the file descriptor
-            created by timerfd_create().  The file descriptor refers to the same
-            underlying  timer  object  as the corresponding file descriptor in
-            the parent, and read(2)s in the child will return information about
-            expirations of the timer.
-
-        execve(2) semantics
-            A file descriptor created by timerfd_create() is preserved across
-            execve(2), and continues to generate timer expirations if the  timer
-            was armed.
-
-        Params:
-            clockid = Specifies the clock  that is used to mark the progress of
-                      the timer, and must be either CLOCK_REALTIME or
-                      CLOCK_MONOTONIC.
-                      - CLOCK_REALTIME is a settable system-wide clock.
-                      - CLOCK_MONOTONIC is a non-settable clock that is not
-                          affected by discontinuous changes in the system clock
-                          (e.g., manual changes to system time). The current
-                          value of each of these clocks can be retrieved using
-                          clock_gettime(2).
-
-            flags   = Starting with Linux 2.6.27: 0 or a bitwise OR combination
-                      of
-                      - TFD_NONBLOCK: Set the O_NONBLOCK file status flag on the
-                            new open file description.
-                      - TFD_CLOEXEC: Set the close-on-exec (FD_CLOEXEC) flag on
-                            the new file descriptor. (See the description of the
-                            O_CLOEXEC  flag  in open(2) for reasons why this may
-                            be useful.)
-
-                      Up to Linux version 2.6.26: Must be 0.
-
-        Returns:
-            a file descriptor that refers to that timer
-
-     **************************************************************************/
-
-    int timerfd_create(int clockid, int flags = 0);
-
-    /**************************************************************************
-
-        Sets next expiration time of interval timer source fd to new_value.
-
-        Params:
-            fd        = file descriptor referring to the timer
-
-            flags     = 0 starts a relative timer using new_value.it_interval;
-                        TFD_TIMER_ABSTIME starts an absolute timer using
-                        new_value.it_value.
-
-            new_value = - it_value: Specifies the initial expiration of the
-                            timer. Setting either field to a non-zero value arms
-                            the timer. Setting both fields to zero disarms the
-                            timer.
-                        - it_interval: Setting one or both fields to non-zero
-                            values specifies the period for repeated timer
-                            expirations after the initial expiration. If both
-                            fields are zero, the timer expires just once, at the
-                            time specified by it_value.
-
-            old_value = Returns the old expiration time as timerfd_gettime().
-
-        Returns:
-            0 on success or -1 on error. Sets errno in case of error.
-
-     **************************************************************************/
-
-    int timerfd_settime(int fd, int flags,
-                        itimerspec* new_value,
-                        itimerspec* old_value);
-
-    /**************************************************************************
-
-        Returns the next expiration time of fd.
-
-        Params:
-            fd         = file descriptor referring to the timer
-            curr_value = - it_value:
-                             Returns the amount of time until the timer will
-                             next expire. If both fields are zero, then the
-                             timer is currently disarmed. Contains always a
-                             relative value, regardless of whether the
-                             TFD_TIMER_ABSTIME flag was specified when setting
-                             the timer.
-                        - it_interval: Returns the interval of the timer. If
-                             both fields are zero, then the timer is set to
-                             expire just once, at the time specified by
-                             it_value.
-
-        Returns:
-            0 on success or -1 on error. Sets errno in case of error.
-
-     **************************************************************************/
-
-    int timerfd_gettime(int fd, itimerspec* curr_value);
-}
+import tango.stdc.posix.time: time_t, timespec, itimerspec;
 
 class TimerEvent : ITimerEvent
 {
@@ -277,6 +119,14 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 {
     /***************************************************************************
 
+        Convenience and compatibility alias.
+
+    ***************************************************************************/
+
+    public alias TimerFD.TimerException TimerException;
+
+    /***************************************************************************
+
         Set to true to use an absolute or false for a relative timer. On default
         a relative timer is used.
 
@@ -291,15 +141,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     ***************************************************************************/
 
-    private int fd;
-
-    /***************************************************************************
-
-        Reused Exception instance
-
-    ***************************************************************************/
-
-    protected TimerException e;
+    private TimerFD fd;
 
     /***********************************************************************
 
@@ -315,11 +157,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     protected this ( bool realtime = false )
     {
-        this.fd = .timerfd_create(realtime? CLOCK_REALTIME : CLOCK_MONOTONIC,
-                                  TFD_NONBLOCK);
-
-        this.e = new TimerException;
-        this.e.enforce(this.fd >= 0, "", identifier!(.timerfd_create));
+        this.fd = new TimerFD(realtime);
     }
 
     /***************************************************************************
@@ -332,17 +170,6 @@ abstract class ITimerEvent : ISelectClient, ISelectable
     public override Event events ( )
     {
         return Event.EPOLLIN;
-    }
-
-    /***********************************************************************
-
-        Destructor. Destroys the file descriptor used to manage the event.
-
-    ***********************************************************************/
-
-    ~this ( )
-    {
-        .close(this.fd);
     }
 
 
@@ -377,11 +204,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     public itimerspec time ( )
     {
-        itimerspec t;
-
-        this.e.enforceRetCode!(.timerfd_gettime)().call(this.fd, &t);
-
-        return t;
+        return this.fd.time;
     }
 
     /***************************************************************************
@@ -405,13 +228,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     public itimerspec set ( timespec first, timespec interval = timespec.init )
     {
-        itimerspec t_new = itimerspec(interval, first),
-                   t_old;
-
-        this.e.enforceRetCode!(.timerfd_settime)().call(
-            this.fd, this.absolute ? TFD_TIMER_ABSTIME : 0, &t_new, &t_old);
-
-        return t_old;
+        return this.fd.set(first, interval);
     }
 
     /***************************************************************************
@@ -446,8 +263,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
     public itimerspec set ( time_t first_s,        uint first_ms,
                             time_t interval_s = 0, uint interval_ms = 0 )
     {
-        return this.set(timespec(first_s,    first_ms    * 1_000_000),
-                        timespec(interval_s, interval_ms * 1_000_000));
+        return this.fd.set(first_s, first_ms, interval_s, interval_ms);
     }
 
     /***************************************************************************
@@ -461,7 +277,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     public itimerspec reset ( )
     {
-        return this.set(timespec.init);
+        return this.fd.reset();
     }
 
     /***********************************************************************
@@ -475,7 +291,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     public override Handle fileHandle ( )
     {
-        return cast(Handle)this.fd;
+        return this.fd.fileHandle;
     }
 
     /***************************************************************************
@@ -492,26 +308,7 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     final override bool handle ( Event event )
     {
-        ulong n;
-
-        if (.read(this.fd, &n, n.sizeof) < 0)
-        {
-            scope (exit) .errno = 0;
-
-            switch (.errno)
-            {
-                case EAGAIN:
-                static if (EAGAIN != EWOULDBLOCK) case EWOULDBLOCK:
-                    return true;
-
-                default:
-                    throw this.e.useGlobalErrno(identifier!(.read));
-            }
-        }
-        else
-        {
-            return this.handle_(n);
-        }
+        return this.handle_(this.fd.handle());
     }
 
     /***************************************************************************
@@ -537,8 +334,4 @@ abstract class ITimerEvent : ISelectClient, ISelectable
             return this.time_buffer;
         }
     }
-
-    /**************************************************************************/
-
-    static class TimerException : ErrnoException { }
 }

commit 5836e68f8796837720f4c4473f3ccae7427585c0

Pass TimerEvent's `absolute` through to underlying TimerFD
The refactoring done by 3e74e183afbdeba1db3432bfb268461d0751f6ec introduced
a bug into TimerEvent, making its `absolute` field ineffectual.

Fixes #783

src/ocean/io/select/client/TimerEvent.d
@@ -125,15 +125,6 @@ abstract class ITimerEvent : ISelectClient, ISelectable
 
     /***************************************************************************
 
-        Set to true to use an absolute or false for a relative timer. On default
-        a relative timer is used.
-
-    ***************************************************************************/
-
-    public bool absolute = false;
-
-    /***************************************************************************
-
         Integer file descriptor provided by the operating system and used to
         manage the custom event.
 
@@ -170,6 +161,33 @@ abstract class ITimerEvent : ISelectClient, ISelectable
         return Event.EPOLLIN;
     }
 
+    /***************************************************************************
+
+        Returns:
+            the value of the TimerFD's absolute flag (true = absolute timer,
+            false = relative timer)
+
+    ***************************************************************************/
+
+    public bool absolute ( )
+    {
+        return this.fd.absolute;
+    }
+
+    /***************************************************************************
+
+        Sets the timer to absolute or relative mode.
+
+        Params:
+            abs = the value of the TimerFD's absolute flag (true = absolute
+                timer, false = relative timer)
+
+    ***************************************************************************/
+
+    public void absolute ( bool abs )
+    {
+        this.fd.absolute = abs;
+    }
 
     /***************************************************************************
 
@@ -336,3 +354,51 @@ abstract class ITimerEvent : ISelectClient, ISelectable
         }
     }
 }
+
+
+version ( UnitTest )
+{
+    import tango.core.Test;
+    import tango.stdc.posix.time;
+
+    extern ( C )
+    {
+        typedef int clockid_t;
+        int clock_gettime(clockid_t, timespec*);
+    }
+
+    class TestTimerEvent : ITimerEvent
+    {
+        override protected bool handle_ ( ulong n )
+        {
+            assert(false);
+        }
+    }
+}
+
+
+/*******************************************************************************
+
+    Test for setting absolute timers
+
+*******************************************************************************/
+
+unittest
+{
+    timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+
+    auto timer = new TestTimerEvent;
+    timer.absolute = true;
+
+    auto set_time = now;
+    set_time.tv_sec += 10;
+    timer.set(set_time);
+
+    auto get_time = timer.time();
+    ulong nsec = (get_time.it_value.tv_sec * 1_000_000_000)
+        + get_time.it_value.tv_nsec;
+
+    test!("<=")(nsec, 10_000_000_000);
+}
+