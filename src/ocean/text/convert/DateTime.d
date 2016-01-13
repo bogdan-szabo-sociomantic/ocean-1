5b3104df8e121f8058ccf2da495c2d4eca605ee6
Gautam Kotian <gautam.kotian@sociomantic.com>
2016-01-13 14:59:46 +0100

    Populate datetime in a separate function
    Populating the datetime struct is the bulk of the processing that is
    performed in the 'timeToUnixTime()' function. Move all that into a
    separate private function. This allows for easy addition of support for
    other formats.

--- src/ocean/text/convert/DateTime.d
+++ src/ocean/text/convert/DateTime.d
@@ -59,9 +59,9 @@ public enum DateConversion : uint
     equivalent unix timestamp. Refer to the 'DateConversion' enum for the list
     of supported formats.
 
-    This method makes use of the C sscanf() function. To use sscanf safely,
-    str needs to be null-terminated. Fortunately the format is clearly defined,
-    so we can copy it into a fixed-length buffer.
+    To call C functions used for the conversion safely, the input string needs
+    to be null-terminated. Fortunately the format is clearly defined, so we can
+    just copy the input string into a fixed-length buffer.
 
     Params:
         str = string containing the timestamp to be converted
@@ -78,13 +78,59 @@ public bool timeToUnixTime ( cstring str, ref time_t time,
     out DateConversion conversion_type )
 {
     tm datetime;
-    char separator;
-    char [("2000-12-12T23:59:59:59Z".length+1)] buff;
+
+    // Create a static buffer of length one more than the longest supported
+    // format
+    char [("2000-12-12T23:59:59:59Z".length + 1)] buff;
 
     auto len = (str.length < buff.length) ? str.length : (buff.length - 1);
 
     buff[0 .. len] = str[0 .. len];
     buff[len] = '\0';
+
+    if ( ! getDateTimeForISO8601Format(buff[0 .. len + 1], str.length, datetime,
+        conversion_type) )
+    {
+        return false;
+    }
+
+    time = timegm(&datetime);
+
+    return true;
+}
+
+
+/*******************************************************************************
+
+    Gets a struct containing details of the date and time corresponding to the
+    given string. This function supports some (but not all) of the formats
+    supported by ISO 8601.
+
+    This method makes use of the C function 'sscanf()', and to call 'sscanf()'
+    safely, the input string must be null-terminated.
+
+    Params:
+        buff = null-terminated string containing the timestamp
+        orig_str_len = length of the original string containing the timestamp
+        datetime = output struct to be filled with details of the date and time
+            corresponding to the timestamp
+        conversion_type = if the timestamp was converted from one of the
+            supported formats, then set the type of conversion used
+
+    Returns:
+        true if the timestamp was successfully converted
+
+*******************************************************************************/
+
+private bool getDateTimeForISO8601Format ( cstring buff, size_t orig_str_len,
+    ref tm datetime, ref DateConversion conversion_type )
+in
+{
+    assert(buff[$ - 1] == '\0', "Input string must be null-terminated");
+}
+body
+{
+    char separator;
 
     // Initialise the time and the day of the month to 0 and 1 respectively
     datetime.tm_hour = datetime.tm_min = datetime.tm_sec = 0;
@@ -98,9 +144,9 @@ public bool timeToUnixTime ( cstring str, ref time_t time,
     Converted: switch ( num_matched )
     {
         case 1:
-            if ( validateCharacters(str) )
+            if ( validateCharacters(buff) )
             {
-                switch ( str.length )
+                switch ( orig_str_len )
                 {
                     case 6: // 201008
                         sscanf(buff.ptr, "%04d%02d".ptr, &datetime.tm_year,
@@ -121,7 +167,7 @@ public bool timeToUnixTime ( cstring str, ref time_t time,
             return false;
 
         case 2:
-            if ( validateCharacters(str, "-") ) // 2010-08
+            if ( validateCharacters(buff, "-") ) // 2010-08
             {
                 conversion_type = DateConversion.YearMonthWithHyphen;
                 break;
@@ -129,7 +175,7 @@ public bool timeToUnixTime ( cstring str, ref time_t time,
             return false;
 
         case 3: // 2013-10-01
-            if ( validateCharacters(str, "-") )
+            if ( validateCharacters(buff, "-") )
             {
                 conversion_type = DateConversion.YearMonthDayWithHyphen;
                 break;
@@ -170,16 +216,15 @@ public bool timeToUnixTime ( cstring str, ref time_t time,
     datetime.tm_mon--;
     datetime.tm_isdst = false;
 
-    time = timegm(&datetime);
-
     return true;
 }
 
 
 /*******************************************************************************
 
-    Check for valid characters in the date string. Valid characters are digits
-    and any characters in the extra parameter.
+    Validates the characters in the given string (until the first '\0' or the
+    end of string is hit). Valid characters are digits and any characters in the
+    extra parameter.
 
     Params:
         str = string to check for valid characters
@@ -195,6 +240,11 @@ private bool validateCharacters ( cstring str, cstring extra = "" )
 {
     foreach ( chr; str )
     {
+        if ( chr == '\0')
+        {
+            break;
+        }
+
         if ( !isDigit(chr) && !extra.contains(chr) )
         {
             return false;
