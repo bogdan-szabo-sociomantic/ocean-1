a40acee51d00ab999a134aec4eecebd5a3f53e5d
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2016-06-14 20:48:22 +0200

    Remove import lines for other OSs
    The implmentation for other OSs was removed anyway.

--- src/ocean/sys/consts/sysctl.d
+++ src/ocean/sys/consts/sysctl.d
@@ -12,15 +12,5 @@
 
 module ocean.sys.consts.sysctl;
 
-version (linux)
-         public import ocean.sys.linux.consts.sysctl;
-else
-version (freebsd)
-         public import ocean.sys.freebsd.consts.sysctl;
-else
-version (darwin)
-         public import ocean.sys.darwin.consts.sysctl;
-else
-version (solaris)
-         public import ocean.sys.solaris.consts.sysctl;
+public import ocean.sys.linux.consts.sysctl;
 
