69b76d813feafabfe54a565eaec1f832c137d481
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2016-06-14 13:53:36 +0200

    Remove old license from DigitalMars
    Instead we move the DigitalMars to the copyright section, in case it
    wasn't there already. All the code affected here was actually written
    either by Walter Bright, Don Clugston and/or Sean Kelly, all of which
    already changed the license to Boost (or Tango) before, so I think is
    safe to assume this code can be released with Tango license without the
    special mentions to the original license pre-Tango/D2.

--- src/ocean/math/Math.d
+++ src/ocean/math/Math.d
@@ -13,38 +13,6 @@
  * Authors: Walter Bright, Don Clugston, Sean Kelly
  *
  */
-/* Portions of this code were taken from Phobos std.math, which has the following
- * copyright notice:
- *
- * Author:
- *  Walter Bright
- * Copyright:
- *  Copyright (c) 2001-2005 by Digital Mars,
- *  All Rights Reserved,
- *  www.digitalmars.com
- * License:
- *  This software is provided 'as-is', without any express or implied
- *  warranty. In no event will the authors be held liable for any damages
- *  arising from the use of this software.
- *
- *  Permission is granted to anyone to use this software for any purpose,
- *  including commercial applications, and to alter it and redistribute it
- *  freely, subject to the following restrictions:
- *
- *  <ul>
- *  <li> The origin of this software must not be misrepresented; you must not
- *       claim that you wrote the original software. If you use this software
- *       in a product, an acknowledgment in the product documentation would be
- *       appreciated but is not required.
- *  </li>
- *  <li> Altered source versions must be plainly marked as such, and must not
- *       be misrepresented as being the original software.
- *  </li>
- *  <li> This notice may not be removed or altered from any source
- *       distribution.
- *  </li>
- *  </ul>
- */
 
 /**
  * Macros:
