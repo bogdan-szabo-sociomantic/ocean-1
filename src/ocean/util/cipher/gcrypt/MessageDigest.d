a151a58611492d0c6f504b44cca894dbc49338d0
davideckardt <david.eckardt@sociomantic.com>
2016-07-25 12:27:14 +0200

    Don't derive HMAC from MessageDigest: Derive both from MessageDigestCore
    Add MessageDigestCore, which contains common core functionality. Prepare
    replacing HMAC.hash() with a method that receives the key as a separate
    argument.

--- src/ocean/util/cipher/gcrypt/MessageDigest.d
+++ src/ocean/util/cipher/gcrypt/MessageDigest.d
@@ -3,11 +3,10 @@
     libgcrypt message digest and HMAC utility classes.
 
     Be aware that not all versions of libcgrypt support all hash algorithms; the
-    `MessageDigest` constructor will throw if the specified algorithm is not
-    supported by the run-time version of libgcrypt. However, if the constructor
-    does not throw, it is safe to assume it will never throw for the same set of
-    parameters (except for the fatal situation that libgcrypt failed allocating
-    memory).
+    constructor will throw if the specified algorithm is not supported by the
+    run-time version of libgcrypt. However, if the constructor does not throw,
+    it is safe to assume it will never throw for the same set of parameters
+    (except for the fatal situation that libgcrypt failed allocating memory).
 
     Requires linking with libgcrypt:
             -L-lgcrypt
@@ -18,20 +17,18 @@
 
 module ocean.util.cipher.gcrypt.MessageDigest;
 
+import ocean.util.cipher.gcrypt.core.MessageDigestCore;
 import ocean.transition;
 
-class MessageDigest
+/*******************************************************************************
+
+    Message digest/hash generator.
+
+*******************************************************************************/
+
+public class MessageDigest: MessageDigestCore
 {
     import ocean.util.cipher.gcrypt.c.md;
-    import ocean.util.cipher.gcrypt.core.Gcrypt: GcryptException;
-
-    /***************************************************************************
-
-        libgcrypt message digest context object.
-
-    ***************************************************************************/
-
-    protected gcry_md_hd_t md;
 
     /***************************************************************************
 
@@ -50,29 +47,9 @@ class MessageDigest
     ***************************************************************************/
 
     public this ( gcry_md_algos algorithm, gcry_md_flags flags = cast(gcry_md_flags)0,
-                  istring file = __FILE__, int line = __LINE__ )
-    out
+                     istring file = __FILE__, int line = __LINE__ )
     {
-        assert(this.md !is null);
-    }
-    body
-    {
-        // `gcry_md_open` sets `this.md = null` on failure.
-        throwIfGcryptError(gcry_md_open(&this.md, algorithm, flags), file, line);
-    }
-
-    /***************************************************************************
-
-        Destructor; closes the object opened by the constructor.
-
-    ***************************************************************************/
-
-    ~this ( )
-    {
-        // `gcry_md_close` ignores `null` so it is safe to call it after
-        // `gcry_md_open()` failed and made the constructor throw.
-        gcry_md_close(this.md);
-        this.md = null;
+        super(algorithm, flags, file, line);
     }
 
     /***************************************************************************
@@ -99,77 +76,37 @@ class MessageDigest
         gcry_md_reset(this.md);
         return this.hash_(input_data);
     }
-
-    /***************************************************************************
-
-        Calculates the hash a.k.a. message digest from the input data.
-
-        The length of the returned hash is the return value of
-        `gcry_md_get_algo_dlen(algorithm)` for the algorithm passed to the
-        constructor of this class.
-
-        Params:
-            input_data = data to hash; the elements will be concatenated
-
-        Returns:
-            the resuting hash.
-
-    ***************************************************************************/
-
-    protected ubyte[] hash_ ( Const!(void)[][] input_data )
-    {
-        foreach (chunk; input_data)
-        {
-            gcry_md_write(this.md, chunk.ptr, chunk.length);
-        }
-
-        return gcry_md_read_slice(this.md);
-    }
-
-    /***************************************************************************
-
-        Throws `new GcryptException` if `error` is not 0.
-
-        Params:
-            error = non-zero error code or 0
-
-        Throws:
-            GcryptException if `error` is not 0.
-
-    ***************************************************************************/
-
-    protected static void throwIfGcryptError ( gcry_error_t error,
-                                               istring file = __FILE__,
-                                               int line = __LINE__ )
-    {
-        if (error)
-        {
-            (new GcryptException).throwIfGcryptError(error, file, line);
-        }
-    }
 }
 
-/******************************************************************************/
+/*******************************************************************************
 
-class HMAC: MessageDigest
+    Hash-based message authentication code generator.
+
+*******************************************************************************/
+
+public class HMAC: MessageDigestCore
 {
+    import ocean.util.cipher.gcrypt.c.md;
+    import ocean.util.cipher.gcrypt.core.Gcrypt: GcryptException;
+
     /***************************************************************************
 
         Constructor.
 
         Params:
             algorithm = the hash algorithm to use
-            flags     = flags to `gcry_md_open()`; `flags.GCRY_MD_FLAG_HMAC` is
-                        added automatically
+            flags     = flags to `gcry_md_open()`
 
         Throws:
-            `GcryptException` on error, including the case that the run-time
-            libgcrypt doesn't support `algorithm` for HMAC calculation.
+            `GcryptException` on error. There are two possible error causes:
+              - The parameters are invalid or not supported by the libcrypt
+                of the run-time enviromnent.
+              - libgcrypt failed allocating memory.
 
     ***************************************************************************/
 
     public this ( gcry_md_algos algorithm, gcry_md_flags flags = cast(gcry_md_flags)0,
-                  istring file = __FILE__, int line = __LINE__ )
+                     istring file = __FILE__, int line = __LINE__ )
     {
         super(algorithm, flags | flags.GCRY_MD_FLAG_HMAC, file, line);
     }
@@ -204,20 +141,23 @@ class HMAC: MessageDigest
 
     ***************************************************************************/
 
-    override public ubyte[] hash ( Const!(void)[][] key_and_input_data ... )
+    public ubyte[] hash ( Const!(void)[][] key_and_input_data ... )
     {
         gcry_md_reset(this.md);
 
         if (key_and_input_data.length)
         {
-            throwIfGcryptError(gcry_md_setkey(this.md,
-                key_and_input_data[0].ptr, key_and_input_data[0].length
-            ));
+            GcryptException.throwNewIfGcryptError(
+                gcry_md_setkey(
+                    this.md,
+                    key_and_input_data[0].ptr, key_and_input_data[0].length
+                )
+            );
+
             return this.hash_(key_and_input_data[1 .. $]);
         }
         else
         {
-            throwIfGcryptError(gcry_md_setkey(this.md, null, 0));
             return this.hash_(null);
         }
     }
