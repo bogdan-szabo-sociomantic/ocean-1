897392325fa74b1f6a5b3a2863fbe07b58e397cb
Nemanja Boric <nemanja.boric@sociomantic.com>
2016-08-12 13:18:54 +0200

    Remove `-----` from the code example
    libddoc parses this as a start of the code example and can't
    proceed anymore. Until libddoc is fixed and relased, we can
    just remove this from the comment.

--- src/ocean/io/console/Tables.d
+++ src/ocean/io/console/Tables.d
@@ -19,7 +19,7 @@
         // A table with 3 columns
         scope table = new Table(3);
 
-        // First row is just a divider (---------------------)
+        // First row is just a divider
         table.firstRow.setDivider();
 
         // Next row contains the headings, a series of strings
@@ -75,7 +75,7 @@
         // A table with 4 columns
         scope table = new Table(4);
 
-        // First row is just a divider (---------------------)
+        // First row is just a divider
         table.firstRow.setDivider();
 
         // Next row contains a hash range occupying two (merged) cells. Note
