eed0c298bb8af76f8215746f3873db671c30318f
Gautam Kotian <gautam.kotian@sociomantic.com>
2016-08-17 18:50:05 +0200

    Minor documentation and formatting fixes

--- src/ocean/text/util/StringC.d
+++ src/ocean/text/util/StringC.d
@@ -1,6 +1,6 @@
-/******************************************************************************
+/*******************************************************************************
 
-    Module for the conversion between strings in C and D. Needed for C library
+    Module for conversion between strings in C and D. Needed for C library
     bindings.
 
     --
@@ -21,20 +21,17 @@
 
     ---
 
-        char[] text;
+        mstring text;
 
         char* cText = StringC.toCString(text);
-        char[] text = StringD.toDString(cText);
+        mstring text = StringD.toDString(cText);
 
     ---
 
-    FIXME: the functions here are not memory safe, they need to be re-written to
-    accept ref char[].
-
- ******************************************************************************/
-
+*******************************************************************************/
 
 module ocean.text.util.StringC;
+
 
 /*******************************************************************************
 
@@ -48,9 +45,15 @@ import ocean.stdc.string: strlen, wcslen;
 import ocean.stdc.stddef: wchar_t;
 
 
+/*******************************************************************************
+
+    Class containing the string conversion functions
+
+*******************************************************************************/
+
 class StringC
 {
-    /**************************************************************************
+    /***************************************************************************
 
         Wide character type alias (platform dependent)
 
@@ -58,7 +61,7 @@ class StringC
 
     public alias wchar_t Wchar;
 
-    /**************************************************************************
+    /***************************************************************************
 
         Null terminators
 
@@ -67,33 +70,11 @@ class StringC
     public const char  Term  = '\0';
     public const Wchar Wterm = '\0';
 
-    /**************************************************************************
+    /***************************************************************************
 
         Converts str to a C string, that is, if a null terminator is not
-        present then it is appended to original string. A pointer to the string
-        is returned.
-
-        Params:
-            str = input string
-
-        Returns:
-            C compatible (null terminated) string
-
-     ***************************************************************************/
-
-    public static char* toCstring (ref char[] str)
-    {
-        if (str.length && !!str[$ - 1])
-            str ~= StringC.Term;
-
-        return str.ptr;
-    }
-
-    /**************************************************************************
-
-        Converts str to a C string, that is, if a null terminator is not
-        present then it is appended to original string. A pointer to the string
-        is returned.
+        present then it is appended to the original string. A pointer to the
+        string is returned.
 
         Params:
             str = input string
@@ -103,49 +84,75 @@ class StringC
 
     ***************************************************************************/
 
-    public static Wchar* toCstring (ref Wchar[] str)
+    public static char* toCstring ( ref char[] str )
     {
         if (str.length && !!str[$ - 1])
-            str ~= StringC.Wterm;
+        {
+            str ~= StringC.Term;
+        }
 
         return str.ptr;
     }
 
-    /**************************************************************************
+    /***************************************************************************
 
-        Converts str to a D string: str is sliced from beginning to its null
-        terminator.
+        Converts str to a C string, that is, if a null terminator is not
+        present then it is appended to the original string. A pointer to the
+        string is returned.
 
         Params:
-            str = C compatible input string (pointer to first element of null
-                  terminated string)
+            str = input string
 
         Returns:
             C compatible (null terminated) string
 
-     ***************************************************************************/
+    ***************************************************************************/
+
+    public static Wchar* toCstring ( ref Wchar[] str )
+    {
+        if (str.length && !!str[$ - 1])
+        {
+            str ~= StringC.Wterm;
+        }
+
+        return str.ptr;
+    }
+
+    /***************************************************************************
+
+        Converts str to a D string: str is sliced from the beginning up to its
+        null terminator.
+
+        Params:
+            str = C compatible input string (pointer to the first character of
+                the null terminated string)
+
+        Returns:
+            D compatible (non-null terminated) string
+
+    ***************************************************************************/
 
     public static cstring toDString ( char* str )
     {
-        return str? str[0 .. strlen(str)] : "";
+        return str ? str[0 .. strlen(str)] : "";
     }
 
-    /**************************************************************************
+    /***************************************************************************
 
-        Converts str to a D string: str is sliced from beginning to its null
-        terminator.
+        Converts str to a D string: str is sliced from the beginning up to its
+        null terminator.
 
         Params:
-            str = C compatible input string (pointer to first element of null
-                  terminated string)
+            str = C compatible input string (pointer to the first character of
+                the null terminated string)
 
         Returns:
-            C compatible (null terminated) string
+            D compatible (non-null terminated) string
 
     ***************************************************************************/
 
     public static Const!(Wchar)[] toDString ( Wchar* str )
     {
-        return str? str[0 .. wcslen(str)] : "";
+        return str ? str[0 .. wcslen(str)] : "";
     }
 }
