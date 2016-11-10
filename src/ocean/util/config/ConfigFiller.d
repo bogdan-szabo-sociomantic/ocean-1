7ec0fac57562674bbfcea29884714640165baa36
Nemanja Boric <nemanja.boric@sociomantic.com>
2016-11-10 12:21:13 +0100

    Change the accessibility level to private for ConfigFiller.readFields
    `readFields` is intented to be accessible only from this module,
    so it should marked as private.

--- src/ocean/util/config/ConfigFiller.d
+++ src/ocean/util/config/ConfigFiller.d
@@ -1040,7 +1040,7 @@ body
 
 *******************************************************************************/
 
-protected void readFieldsImpl ( T, Source )
+private void readFieldsImpl ( T, Source )
                           ( cstring group, ref T reference, Source config )
 in
 {
@@ -1119,7 +1119,7 @@ body
 
 *******************************************************************************/
 
-protected void readFields ( T : Object, Source )
+private void readFields ( T : Object, Source )
                           ( cstring group, T reference, Source config )
 {
     // Workaround to work on both l- and r-values
@@ -1142,7 +1142,7 @@ protected void readFields ( T : Object, Source )
 
 *******************************************************************************/
 
-protected void readFields ( T, Source )
+private void readFields ( T, Source )
                           ( cstring group, ref T reference, Source config )
 {
     readFieldsImpl(group, reference, config);
